7cf176e0b02da90ac506b8790cd1716203dba76f
LiangYue <LiangYue1981816@Hotmail.com>
2018-11-16 19:52:48 +0800

    - 同步代码

--- Project/jni/ENGINE_INCLUDES.mk
+++ Project/jni/ENGINE_INCLUDES.mk
@@ -3,23 +3,29 @@ ENGINE_INCLUDES := \
 	$(LOCAL_PATH)/../../ThirdParty/event \
 	$(LOCAL_PATH)/../../ThirdParty/gl3w \
 	$(LOCAL_PATH)/../../ThirdParty/lodepng \
+	$(LOCAL_PATH)/../../ThirdParty/rbtree \
 	$(LOCAL_PATH)/../../ThirdParty/spirv-cross \
 	$(LOCAL_PATH)/../../ThirdParty/tinyxml \
-	$(LOCAL_PATH)/../../ThirdParty/rbtree \
 	$(LOCAL_PATH)/../../ThirdParty/zlib \
 	$(LOCAL_PATH)/../../Engine \
-	$(LOCAL_PATH)/../../Engine/Base \
-	$(LOCAL_PATH)/../../Engine/Memory \
-	$(LOCAL_PATH)/../../Engine/Renderer \
-	$(LOCAL_PATH)/../../Engine/Renderer/Compiler \
-	$(LOCAL_PATH)/../../Engine/Renderer/Gfx \
-	$(LOCAL_PATH)/../../Engine/Renderer/GLES3 \
-	$(LOCAL_PATH)/../../Engine/Renderer/Vulkan \
-	$(LOCAL_PATH)/../../Engine/RenderSolution \
-	$(LOCAL_PATH)/../../Engine/RenderSolution/Task \
-	$(LOCAL_PATH)/../../Engine/Scene \
-	$(LOCAL_PATH)/../../Engine/Scene/Task \
-	$(LOCAL_PATH)/../../Engine/Scene/Component \
+	$(LOCAL_PATH)/../../Engine/Base/include \
+	$(LOCAL_PATH)/../../Engine/Base/src \
+	$(LOCAL_PATH)/../../Engine/Memory/include \
+	$(LOCAL_PATH)/../../Engine/Memory/src \
+	$(LOCAL_PATH)/../../Engine/Renderer/Compiler/include \
+	$(LOCAL_PATH)/../../Engine/Renderer/Compiler/src \
+	$(LOCAL_PATH)/../../Engine/Renderer/Gfx/include \
+	$(LOCAL_PATH)/../../Engine/Renderer/Gfx/src \
+	$(LOCAL_PATH)/../../Engine/Renderer/GLES3/include \
+	$(LOCAL_PATH)/../../Engine/Renderer/GLES3/src \
+	$(LOCAL_PATH)/../../Engine/Renderer/Vulkan/include \
+	$(LOCAL_PATH)/../../Engine/Renderer/Vulkan/src \
+	$(LOCAL_PATH)/../../Engine/RenderSolution/include \
+	$(LOCAL_PATH)/../../Engine/RenderSolution/src \
+	$(LOCAL_PATH)/../../Engine/Scene/include \
+	$(LOCAL_PATH)/../../Engine/Scene/src \
+	$(LOCAL_PATH)/../../Engine/SceneComponent/include \
+	$(LOCAL_PATH)/../../Engine/SceneComponent/src \
 	$(LOCAL_PATH)/../../Engine/ThirdParty \
 	$(LOCAL_PATH)/../../Engine/ThirdParty/eastl \
 	$(LOCAL_PATH)/../../Engine/ThirdParty/gli \
