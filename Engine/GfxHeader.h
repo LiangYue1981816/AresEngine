6f2d4579e24e1c1da460b490009556a5143d2366
LiangYue <LiangYue1981816@Hotmail.com>
2018-12-09 14:06:23 +0800

    - 调整代码

--- Engine/GfxHeader.h
+++ Engine/GfxHeader.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "PreHeader.h"
 
-#include "gles3/gl31.h"
 #include "ThirdParty/gli/gli.hpp"
 #include "ThirdParty/spirv-cross/spirv_cfg.hpp"
 #include "ThirdParty/spirv-cross/spirv_msl.hpp"
@@ -13,6 +12,5 @@
 #include "Base/include/Utils.h"
 #include "Base/include/TaskGraph.h"
 
-#include "Renderer/Compiler/include/ShaderCompiler.h"
 #include "Renderer/Gfx/include/GfxRenderer.h"
-#include "Renderer/GLES3/include/GLES3Renderer.h"
+#include "Renderer/Compiler/include/ShaderCompiler.h"
