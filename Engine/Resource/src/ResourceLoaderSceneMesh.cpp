b26c4e1b64ab52a944849c6cbd4fd57cfc19dfb4
LiangYue <LiangYue1981816@Hotmail.com>
2018-12-13 11:26:18 +0800

    - 调整代码

--- Engine/Resource/src/ResourceLoaderSceneMesh.cpp
+++ Engine/Resource/src/ResourceLoaderSceneMesh.cpp
@@ -6,7 +6,7 @@
 #include "FileManager.h"
 
 
-static bool LoadDraw(TiXmlNode *pNode, const CGfxMeshPtr &ptrMesh, CSceneNode *pCurrentSceneNode)
+static bool LoadDraw(TiXmlNode *pNode, const CGfxMeshPtr &ptrMesh, CSceneNode *pCurrentSceneNode, uint32_t instanceBinding, uint32_t instanceFormat)
 {
 	int err = 0;
 
@@ -30,7 +30,7 @@ ERR:
 	return false;
 }
 
-static bool LoadNode(TiXmlNode *pNode, const CGfxMeshPtr &ptrMesh, CSceneNode *pParentSceneNode)
+static bool LoadNode(TiXmlNode *pNode, const CGfxMeshPtr &ptrMesh, CSceneNode *pParentSceneNode, uint32_t instanceBinding, uint32_t instanceFormat)
 {
 	int err = 0;
 	CSceneNode *pCurrentSceneNode = nullptr;
@@ -49,7 +49,7 @@ static bool LoadNode(TiXmlNode *pNode, const CGfxMeshPtr &ptrMesh, CSceneNode *p
 			pCurrentSceneNode->SetLocalPosition(translation[0], translation[1], translation[2]);
 		}
 
-		if (LoadDraw(pNode, ptrMesh, pCurrentSceneNode) == false) {
+		if (LoadDraw(pNode, ptrMesh, pCurrentSceneNode, instanceBinding, instanceFormat) == false) {
 			err = -1; goto ERR;
 		}
 
@@ -59,7 +59,7 @@ static bool LoadNode(TiXmlNode *pNode, const CGfxMeshPtr &ptrMesh, CSceneNode *p
 
 		if (TiXmlNode *pChildNode = pNode->FirstChild("Node")) {
 			do {
-				if (LoadNode(pChildNode, ptrMesh, pCurrentSceneNode) == false) {
+				if (LoadNode(pChildNode, ptrMesh, pCurrentSceneNode, instanceBinding, instanceFormat) == false) {
 					err = -3; goto ERR;
 				}
 			} while ((pChildNode = pNode->IterateChildren("Node", pChildNode)) != nullptr);
@@ -71,7 +71,7 @@ ERR:
 	return false;
 }
 
-static bool LoadMesh(TiXmlNode *pMeshNode, CSceneNode *pParentSceneNode, uint32_t instanceFormat)
+static bool LoadMesh(TiXmlNode *pMeshNode, CSceneNode *pParentSceneNode, uint32_t vertexBinding, uint32_t instanceBinding, uint32_t instanceFormat)
 {
 	int err = 0;
 	{
@@ -79,11 +79,11 @@ static bool LoadMesh(TiXmlNode *pMeshNode, CSceneNode *pParentSceneNode, uint32_
 			const char *szMeshFileName = pMeshNode->ToElement()->AttributeString("mesh");
 			if (szMeshFileName == nullptr) { err = -1; goto ERR; }
 
-			CGfxMeshPtr ptrMesh = GfxRenderer()->NewMesh(szMeshFileName, instanceFormat);
+			CGfxMeshPtr ptrMesh = GfxRenderer()->NewMesh(szMeshFileName, vertexBinding);
 			if (ptrMesh.IsValid() == false) { err = -2; goto ERR; }
 
 			do {
-				if (LoadNode(pNode, ptrMesh, pParentSceneNode) == false) { err = -3; goto ERR; }
+				if (LoadNode(pNode, ptrMesh, pParentSceneNode, instanceBinding, instanceFormat) == false) { err = -3; goto ERR; }
 			} while ((pNode = pMeshNode->IterateChildren("Node", pNode)) != nullptr);
 		}
 	}
@@ -92,7 +92,7 @@ ERR:
 	return false;
 }
 
-static CSceneNode* LoadMesh(const char *szFileName, CSceneNode *pParentSceneNode, uint32_t instanceFormat)
+static CSceneNode* LoadMesh(const char *szFileName, CSceneNode *pParentSceneNode, uint32_t vertexBinding, uint32_t instanceBinding, uint32_t instanceFormat)
 {
 	/*
 	<Mesh mesh="sponza.mesh">
@@ -117,7 +117,7 @@ static CSceneNode* LoadMesh(const char *szFileName, CSceneNode *pParentSceneNode
 		if (pMeshNode == nullptr) { err = -3; goto ERR; }
 
 		pCurrentSceneNode = pParentSceneNode->GetSceneManager()->CreateNode(pParentSceneNode->GetSceneManager()->GetNextNodeName());
-		if (LoadMesh(pMeshNode, pCurrentSceneNode, instanceFormat) == false) { err = -4; goto ERR; }
+		if (LoadMesh(pMeshNode, pCurrentSceneNode, vertexBinding, instanceBinding, instanceFormat) == false) { err = -4; goto ERR; }
 		if (pParentSceneNode->AttachNode(pCurrentSceneNode) == false) { err = -5; goto ERR; }
 	}
 	return pCurrentSceneNode;
@@ -126,7 +126,7 @@ ERR:
 	return nullptr;
 }
 
-CSceneNode* CResourceLoader::LoadSceneMesh(const char *szFileName, CSceneNode *pParentSceneNode, uint32_t instanceFormat)
+CSceneNode* CResourceLoader::LoadSceneMesh(const char *szFileName, CSceneNode *pParentSceneNode, uint32_t vertexBinding, uint32_t instanceBinding, uint32_t instanceFormat)
 {
-	return ::LoadMesh(szFileName, pParentSceneNode, instanceFormat);
+	return ::LoadMesh(szFileName, pParentSceneNode, vertexBinding, instanceBinding, instanceFormat);
 }
