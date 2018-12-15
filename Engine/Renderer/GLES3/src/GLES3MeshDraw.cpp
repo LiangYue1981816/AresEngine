41c2a4b114fedccbfddcf337c534dd2a74418ee1
LiangYue <LiangYue1981816@Hotmail.com>
2018-12-15 14:17:40 +0800

    - 调整代码

--- Engine/Renderer/GLES3/src/GLES3MeshDraw.cpp
+++ Engine/Renderer/GLES3/src/GLES3MeshDraw.cpp
@@ -10,15 +10,17 @@ CGLES3MeshDraw::CGLES3MeshDraw(CGLES3MeshDrawManager *pManager, uint32_t name, c
 	, m_pVertexArrayObject(nullptr)
 	, m_pDrawIndirectBuffer(nullptr)
 {
-	m_ptrMesh = ptrMesh;
-	m_pMeshDraw = ptrMesh->GetDraw(indexDraw);
+	if (ptrMesh.IsValid() && ptrMesh->GetDraw(indexDraw) != nullptr) {
+		m_ptrMesh = ptrMesh;
+		m_pMeshDraw = ptrMesh->GetDraw(indexDraw);
 
-	m_pInstanceBuffer = new CGLES3InstanceBuffer(instanceFormat, instanceBinding);
-	m_pVertexArrayObject = new CGLES3VertexArrayObject;
-	m_pVertexArrayObject->Buffer((CGLES3IndexBuffer*)ptrMesh->GetIndexBuffer(), (CGLES3VertexBuffer *)ptrMesh->GetVertexBuffer(), m_pInstanceBuffer);
+		m_pInstanceBuffer = new CGLES3InstanceBuffer(instanceFormat, instanceBinding);
+		m_pVertexArrayObject = new CGLES3VertexArrayObject;
+		m_pVertexArrayObject->Buffer((CGLES3IndexBuffer*)ptrMesh->GetIndexBuffer(), (CGLES3VertexBuffer *)ptrMesh->GetVertexBuffer(), m_pInstanceBuffer);
 
-	m_pDrawIndirectBuffer = new CGLES3DrawIndirectBuffer(1);
-	m_pDrawIndirectBuffer->BufferData(0, m_pMeshDraw->baseVertex, m_pMeshDraw->firstIndex, m_pMeshDraw->indexCount, 0);
+		m_pDrawIndirectBuffer = new CGLES3DrawIndirectBuffer(1);
+		m_pDrawIndirectBuffer->BufferData(0, m_pMeshDraw->baseVertex, m_pMeshDraw->firstIndex, m_pMeshDraw->indexCount, 0);
+	}
 }
 
 CGLES3MeshDraw::~CGLES3MeshDraw(void)
