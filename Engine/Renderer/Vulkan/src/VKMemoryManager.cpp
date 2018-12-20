f3c2125b82b9c48fded672d1732d4f49227501b1
LiangYue <LiangYue1981816@Hotmail.com>
2018-12-20 14:21:50 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VKMemoryManager.cpp
+++ Engine/Renderer/Vulkan/src/VKMemoryManager.cpp
@@ -1,1 +1,14 @@
 #include "VKRenderer.h"
+
+
+static int GetMemoryTypeIndex(const VkPhysicalDeviceMemoryProperties &memoryProperties, VkFlags memoryTypeBits, VkMemoryPropertyFlags memoryPropertyFlags)
+{
+	for (int indexMemoryType = 0; indexMemoryType < memoryProperties.memoryTypeCount; indexMemoryType++) {
+		if ((memoryTypeBits & (1 << indexMemoryType)) &&
+			(memoryProperties.memoryTypes[indexMemoryType].propertyFlags & memoryPropertyFlags) == memoryPropertyFlags) {
+			return indexMemoryType;
+		}
+	}
+
+	return -1;
+}
