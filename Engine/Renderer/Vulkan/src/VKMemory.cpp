c95ef362c74cdb0a4d7c980a2bc39adcb68df060
LiangYue <LiangYue1981816@Hotmail.com>
2019-01-04 00:10:09 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VKMemory.cpp
+++ Engine/Renderer/Vulkan/src/VKMemory.cpp
@@ -39,7 +39,6 @@ bool CVKMemory::BindImage(VkImage vkImage) const
 	VkMemoryRequirements requirements;
 	vkGetImageMemoryRequirements(m_pDevice->GetDevice(), vkImage, &requirements);
 	ASSERT(m_size >= requirements.size);
-	ASSERT(ALIGN_BYTE(m_size, requirements.alignment) == m_size);
 	ASSERT(ALIGN_BYTE(m_offset + m_alignmentOffset, requirements.alignment) == m_offset + m_alignmentOffset);
 #endif
 	CALL_VK_FUNCTION_RETURN_BOOL(vkBindImageMemory(m_pDevice->GetDevice(), vkImage, m_vkMemory, m_offset + m_alignmentOffset));
@@ -52,7 +51,6 @@ bool CVKMemory::BindBuffer(VkBuffer vkBuffer) const
 	VkMemoryRequirements requirements;
 	vkGetBufferMemoryRequirements(m_pDevice->GetDevice(), vkBuffer, &requirements);
 	ASSERT(m_size >= requirements.size);
-	ASSERT(ALIGN_BYTE(m_size, requirements.alignment) == m_size);
 	ASSERT(ALIGN_BYTE(m_offset + m_alignmentOffset, requirements.alignment) == m_offset + m_alignmentOffset);
 #endif
 	CALL_VK_FUNCTION_RETURN_BOOL(vkBindBufferMemory(m_pDevice->GetDevice(), vkBuffer, m_vkMemory, m_offset + m_alignmentOffset));
