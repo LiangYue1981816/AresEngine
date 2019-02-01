5727a28ef3464782fd671790030ca0b09c64b01f
LiangYue <LiangYue1981816@Hotmail.com>
2019-02-01 16:17:48 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/include/VkDescriptorSet.h
+++ Engine/Renderer/Vulkan/include/VkDescriptorSet.h
@@ -2,3 +2,13 @@
 #include "VKRenderer.h"
 
 
+class CVKDescriptorSet
+{
+private:
+	CVKDescriptorSet(CVKDevice *pDevice, VkDescriptorSet vkDescriptorSet);
+	virtual ~CVKDescriptorSet(void);
+
+
+private:
+	CVKDevice *m_pDevice;
+};
