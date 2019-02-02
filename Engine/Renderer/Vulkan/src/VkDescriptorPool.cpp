b2b7597c62e87e83e968ec32d74f96e18c0d6e13
liangyue <liangyue1981816@hotmail.com>
2019-02-02 23:46:31 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VkDescriptorPool.cpp
+++ Engine/Renderer/Vulkan/src/VkDescriptorPool.cpp
@@ -1,26 +1,35 @@
 #include "VKRenderer.h"
 
 
+const uint32_t numMaxSets = 256;
+const uint32_t numUniformBuffers = 1024;
+const uint32_t numCombinedImageSamplers = 512;
+const uint32_t numStorageImages = 32;
+const uint32_t numStorageBuffers = 32;
+const uint32_t numStorageTexelBuffers = 32;
+const uint32_t numUniformTexelBuffers = 32;
+const uint32_t numInputAttachments = 32;
+
 CVKDescriptorPool::CVKDescriptorPool(CVKDevice *pDevice)
 	: m_pDevice(pDevice)
 	, m_vkDescriptorPool(VK_NULL_HANDLE)
 {
 	VkDescriptorPoolSize poolSizes[] = {
-		{ VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1024 },
-		{ VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1024 },
-		{ VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 512 },
-		{ VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 32 },
-		{ VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 32 },
-		{ VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 32 },
-		{ VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 32 },
-		{ VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 32 },
+		{ VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, numUniformBuffers },
+		{ VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, numUniformBuffers },
+		{ VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, numCombinedImageSamplers },
+		{ VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, numStorageImages },
+		{ VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, numStorageBuffers },
+		{ VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, numStorageTexelBuffers },
+		{ VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, numUniformTexelBuffers },
+		{ VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, numInputAttachments },
 	};
 
 	VkDescriptorPoolCreateInfo createInfo = {};
 	createInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
 	createInfo.pNext = nullptr;
 	createInfo.flags = 0;
-	createInfo.maxSets = 256;
+	createInfo.maxSets = numMaxSets;
 	createInfo.poolSizeCount = sizeof(poolSizes) / sizeof(VkDescriptorPoolSize);
 	createInfo.pPoolSizes = poolSizes;
 	vkCreateDescriptorPool(m_pDevice->GetDevice(), &createInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkDescriptorPool);
