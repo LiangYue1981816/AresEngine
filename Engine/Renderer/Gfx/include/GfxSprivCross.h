b3d0e75c04a2ec70a6319b6aac051b38bbe65e5b
liangyue <liangyue1981816@hotmail.com>
2019-02-17 15:42:54 +0800

    + 添加SprivCross类

---
+++ Engine/Renderer/Gfx/include/GfxSprivCross.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "GfxRenderer.h"
+
+
+typedef struct BufferRange {
+	uint32_t offset;
+	uint32_t range;
+} BufferRange;
+
+class CALL_API CGfxSprivCross
+{
+public:
+	CGfxSprivCross(const uint32_t *words, size_t numWords, uint32_t version = 450);
+	virtual ~CGfxSprivCross(void);
+
+
+public:
+	const eastl::string& GetSource(void) const;
+	const eastl::unordered_map<eastl::string, BufferRange>& GetPushConstantRanges(void) const;
+	const eastl::unordered_map<eastl::string, uint32_t>& GetUniformBlockBindings(void) const;
+	const eastl::unordered_map<eastl::string, uint32_t>& GetSampledImageBindings(void) const;
+	const eastl::unordered_map<eastl::string, uint32_t>& GetInputAttachmentBindings(void) const;
+
+
+private:
+	eastl::string m_source;
+	eastl::unordered_map<eastl::string, BufferRange> m_pushConstantRanges;
+	eastl::unordered_map<eastl::string, uint32_t> m_uniformBlockBindings;
+	eastl::unordered_map<eastl::string, uint32_t> m_sampledImageBindings;
+	eastl::unordered_map<eastl::string, uint32_t> m_inputAttachmentBindings;
+};
