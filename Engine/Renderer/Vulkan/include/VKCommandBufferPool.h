924c3cc4859f4d6e38f7e81234b64fb94c41d52f
LiangYue <LiangYue1981816@Hotmail.com>
2019-02-28 14:34:48 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/include/VKCommandBufferPool.h
+++ Engine/Renderer/Vulkan/include/VKCommandBufferPool.h
@@ -2,3 +2,19 @@
 #include "VKRenderer.h"
 
 
+class CVKCommandBufferPool
+{
+	friend class CVKCommandBufferManager;
+
+
+private:
+	CVKCommandBufferPool(CVKDevice *pDevice);
+	virtual ~CVKCommandBufferPool(void);
+
+
+private:
+	VkCommandPool m_vkCommandPool;
+
+private:
+	CVKDevice *m_pDevice;
+};
