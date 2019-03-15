2f54f495d0f0aba230fa2e11605489ce147b0ae9
LiangYue <liangyue1981816@hotmail.com>
2019-03-15 23:43:30 +0800

    - 完善GLES3Buffer类

--- Engine/Renderer/GLES3/include/GLES3Buffer.h
+++ Engine/Renderer/GLES3/include/GLES3Buffer.h
@@ -4,13 +4,26 @@
 
 class CGLES3Buffer
 {
-protected:
+private:
 	CGLES3Buffer(uint32_t target, size_t size, bool bDynamic);
 	virtual ~CGLES3Buffer(void);
 
 
-protected:
+private:
+	uint32_t GetSize(void) const;
+
+private:
+	bool BufferData(size_t offset, size_t size, const void *pBuffer);
+
+private:
+	void Bind(void) const;
+	void Bind(int index, int offset, int size) const;
+
+
+private:
 	uint32_t m_size;
 	uint32_t m_target;
 	uint32_t m_buffer;
 };
+
+typedef CGfxResourcePtr<CGLES3Buffer> CGLES3BufferPtr;
--- Engine/Renderer/GLES3/src/GLES3Buffer.cpp
+++ Engine/Renderer/GLES3/src/GLES3Buffer.cpp
@@ -16,3 +16,35 @@ CGLES3Buffer::~CGLES3Buffer(void)
 {
 	glDeleteBuffers(1, &m_buffer);
 }
+
+uint32_t CGLES3Buffer::GetSize(void) const
+{
+	return m_size;
+}
+
+bool CGLES3Buffer::BufferData(size_t offset, size_t size, const void *pBuffer)
+{
+	if (m_size < (uint32_t)(offset + size)) {
+		return false;
+	}
+
+	glBindBuffer(m_target, m_buffer);
+	glBufferSubData(m_target, (int)offset, (uint32_t)size, pBuffer);
+	glBindBuffer(m_target, 0);
+
+	return true;
+}
+
+void CGLES3Buffer::Bind(void) const
+{
+	GLBindBuffer(m_target, m_buffer);
+}
+
+void CGLES3Buffer::Bind(int index, int offset, int size) const
+{
+	if (m_size < (uint32_t)(offset + size)) {
+		return;
+	}
+
+	GLBindBufferRange(m_target, index, m_buffer, offset, size);
+}
