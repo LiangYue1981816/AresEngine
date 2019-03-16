474e5c806e8e5713a3e57583e832665b4ad8c22f
LiangYue <LiangYue1981816@Hotmail.com>
2019-03-16 10:19:15 +0800

    - 调整代码

--- Engine/Renderer/GLES3/include/GLES3Texture.h
+++ Engine/Renderer/GLES3/include/GLES3Texture.h
@@ -4,6 +4,12 @@
 
 class CGLES3Texture : public CGfxResource
 {
+	friend class CGLES3RenderTexture;
+	friend class CGLES3Texture2D;
+	friend class CGLES3Texture2DArray;
+	friend class CGLES3TextureCubeMap;
+
+
 private:
 	CGLES3Texture(void);
 	virtual ~CGLES3Texture(void);
