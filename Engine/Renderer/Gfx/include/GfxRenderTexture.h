6d8526fdc003b0e1d796f8d81ea27069c055b8b3
liangyue <liangyue1981816@hotmail.com>
2019-03-17 13:48:09 +0800

    - 调整代码

--- Engine/Renderer/Gfx/include/GfxRenderTexture.h
+++ Engine/Renderer/Gfx/include/GfxRenderTexture.h
@@ -30,6 +30,7 @@ public:
 	virtual int GetSamples(void) const = 0;
 
 public:
+	virtual bool Create(HANDLE hExternalTexture, GfxPixelFormat format, int width, int height, int samples = 1) = 0;
 	virtual bool Create(GfxPixelFormat format, int width, int height, int samples = 1, bool bTransient = false) = 0;
 	virtual void Destroy(void) = 0;
 };
