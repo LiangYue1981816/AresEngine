391cfffd03801a988f114aa61e2bdb33f8f6aafc
liangyue <liangyue1981816@hotmail.com>
2019-03-26 23:57:51 +0800

    - 调整代码

--- Engine/Renderer/GLES3/include/GLES3CommandBuffer.h
+++ Engine/Renderer/GLES3/include/GLES3CommandBuffer.h
@@ -15,10 +15,7 @@ private:
 
 public:
 	HANDLE GetCommandBuffer(void) const;
-
-public:
 	bool IsMainCommandBuffer(void) const;
-	uint32_t GetCommandCount(void) const;
 
 public:
 	void Clearup(void);
@@ -28,55 +25,56 @@ public:
 	bool CmdBeginRenderPass(const CGfxFrameBufferPtr ptrFrameBuffer, const CGfxRenderPassPtr ptrRenderPass);
 	bool CmdNextSubpass(void);
 	bool CmdEndRenderPass(void);
+
 	bool CmdBindPipelineCompute(const CGfxPipelineCompute *pPipelineCompute);
 	bool CmdBindPipelineGraphics(const CGfxPipelineGraphics *pPipelineGraphics);
-	bool CmdBindMaterialPass(const CGfxMaterialPtr ptrMaterial, uint32_t namePass);
-	bool CmdBindUniformBuffer(const CGfxUniformBufferPtr ptrUniformBuffer, uint32_t nameUniform);
-	bool CmdBindMeshDraw(const CGfxMeshDrawPtr ptrMeshDraw);
-	bool CmdUniform1i(const char *szName, int v0);
-	bool CmdUniform2i(const char *szName, int v0, int v1);
-	bool CmdUniform3i(const char *szName, int v0, int v1, int v2);
-	bool CmdUniform4i(const char *szName, int v0, int v1, int v2, int v3);
-	bool CmdUniform1f(const char *szName, float v0);
-	bool CmdUniform2f(const char *szName, float v0, float v1);
-	bool CmdUniform3f(const char *szName, float v0, float v1, float v2);
-	bool CmdUniform4f(const char *szName, float v0, float v1, float v2, float v3);
-	bool CmdUniform1iv(const char *szName, int count, const int *value);
-	bool CmdUniform2iv(const char *szName, int count, const int *value);
-	bool CmdUniform3iv(const char *szName, int count, const int *value);
-	bool CmdUniform4iv(const char *szName, int count, const int *value);
-	bool CmdUniform1fv(const char *szName, int count, const float *value);
-	bool CmdUniform2fv(const char *szName, int count, const float *value);
-	bool CmdUniform3fv(const char *szName, int count, const float *value);
-	bool CmdUniform4fv(const char *szName, int count, const float *value);
-	bool CmdUniformMatrix2fv(const char *szName, int count, const float *value);
-	bool CmdUniformMatrix3fv(const char *szName, int count, const float *value);
-	bool CmdUniformMatrix4fv(const char *szName, int count, const float *value);
-	bool CmdClearDepth(float depth);
-	bool CmdClearColor(float red, float green, float blue, float alpha);
+	bool CmdBindDescriptorSet(const CGfxDescriptorSetPtr ptrDescriptorSet);
+
+	bool CmdUniform1i(uint32_t name, int v0);
+	bool CmdUniform2i(uint32_t name, int v0, int v1);
+	bool CmdUniform3i(uint32_t name, int v0, int v1, int v2);
+	bool CmdUniform4i(uint32_t name, int v0, int v1, int v2, int v3);
+	bool CmdUniform1f(uint32_t name, float v0);
+	bool CmdUniform2f(uint32_t name, float v0, float v1);
+	bool CmdUniform3f(uint32_t name, float v0, float v1, float v2);
+	bool CmdUniform4f(uint32_t name, float v0, float v1, float v2, float v3);
+	bool CmdUniform1iv(uint32_t name, int count, const int *value);
+	bool CmdUniform2iv(uint32_t name, int count, const int *value);
+	bool CmdUniform3iv(uint32_t name, int count, const int *value);
+	bool CmdUniform4iv(uint32_t name, int count, const int *value);
+	bool CmdUniform1fv(uint32_t name, int count, const float *value);
+	bool CmdUniform2fv(uint32_t name, int count, const float *value);
+	bool CmdUniform3fv(uint32_t name, int count, const float *value);
+	bool CmdUniform4fv(uint32_t name, int count, const float *value);
+	bool CmdUniformMatrix2fv(uint32_t name, int count, const float *value);
+	bool CmdUniformMatrix3fv(uint32_t name, int count, const float *value);
+	bool CmdUniformMatrix4fv(uint32_t name, int count, const float *value);
+
 	bool CmdSetScissor(int x, int y, int width, int height);
 	bool CmdSetViewport(int x, int y, int width, int height);
-	bool CmdSetInstanceBufferData(const CGfxMeshDrawPtr ptrMeshDraw, const uint8_t *pInstanceBuffer, uint32_t size);
-	bool CmdDrawElements(const CGfxMeshDrawPtr ptrMeshDraw);
-	bool CmdDrawInstance(const CGfxMeshDrawPtr ptrMeshDraw);
-	bool CmdDrawIndirect(const CGfxMeshDrawPtr ptrMeshDraw);
+
+	bool CmdClearDepth(float depth);
+	bool CmdClearColor(float red, float green, float blue, float alpha);
+
+	bool CmdDrawInstance(const CGfxMeshDrawPtr ptrMeshDraw, const uint8_t *pInstanceBuffer, uint32_t size);
+	bool CmdDrawIndirect(const CGfxMeshDrawPtr ptrMeshDraw, const uint8_t *pInstanceBuffer, uint32_t size);
+
 	bool CmdExecute(const CGfxCommandBufferPtr ptrCommandBuffer);
 	bool CmdPresent(void);
+
 	bool CmdPushDebugGroup(const char *szMessage);
 	bool CmdPopDebugGroup(void);
 
 
 private:
 	bool m_bMainCommandBuffer;
+	eastl::vector<CGfxCommandBase*> m_pCommands;
 
 private:
 	bool m_bInRenderPass;
 	uint32_t m_indexSubpass;
 	CGfxRenderPassPtr m_ptrRenderPass;
 	CGfxFrameBufferPtr m_ptrFrameBuffer;
-
-private:
-	eastl::vector<CGfxCommandBase*> m_pCommands;
 
 private:
 	CGLES3CommandBufferManager *m_pManager;
