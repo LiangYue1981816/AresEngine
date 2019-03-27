0371d5baef90f3b2d0bd4078715aca60d6d1b497
liangyue <liangyue1981816@hotmail.com>
2019-03-27 10:33:00 +0800

    - 调整代码

--- Engine/Renderer/GLES3/src/GLES3SamplerManager.cpp
+++ Engine/Renderer/GLES3/src/GLES3SamplerManager.cpp
@@ -15,10 +15,10 @@ CGLES3SamplerManager::~CGLES3SamplerManager(void)
 
 CGLES3Sampler* CGLES3SamplerManager::Create(GfxFilter minFilter, GfxFilter magFilter, GfxSamplerMipmapMode mipmapMode, GfxSamplerAddressMode addressMode)
 {
+	uint32_t name = HashValueFormat("%x_%x_%x_%x", minFilter, magFilter, mipmapMode, addressMode);
+
 	mutex_autolock autolock(&lock);
 	{
-		uint32_t name = HashValueFormat("%x_%x_%x_%x", minFilter, magFilter, mipmapMode, addressMode);
-
 		if (m_pSamplers[name] == nullptr) {
 			m_pSamplers[name] = new CGLES3Sampler(minFilter, magFilter, mipmapMode, addressMode);
 		}
