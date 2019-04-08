fb60638b56182c3e342d1c0e66de15e182c8f31b
LiangYue <liangyue1981816@hotmail.com>
2019-04-08 23:18:19 +0800

    - 调整代码

--- Engine/Renderer/Gfx/include/GfxRenderer.h
+++ Engine/Renderer/Gfx/include/GfxRenderer.h
@@ -59,17 +59,17 @@
 #define GfxRenderer() CGfxRenderer::GetInstance()
 
 
-#define DESCRIPTOR_SET_ENGINE            0
-#define DESCRIPTOR_SET_CAMERA            1
-#define DESCRIPTOR_SET_PASS              2
-#define DESCRIPTOR_SET_INPUTATTACHMENT   3
-#define DESCRIPTOR_SET_COUNT             4
+#define DESCRIPTOR_SET_ENGINE               0
+#define DESCRIPTOR_SET_CAMERA               1
+#define DESCRIPTOR_SET_PASS                 2
+#define DESCRIPTOR_SET_INPUTATTACHMENT      3
+#define DESCRIPTOR_SET_COUNT                4
 
-#define DESCRIPTOR_BIND_ENGINE           0
-#define DESCRIPTOR_BIND_CAMERA           1
+#define DESCRIPTOR_BIND_ENGINE              0
+#define DESCRIPTOR_BIND_CAMERA              1
 
-#define UNIFORM_ENGINE_NAME              HashValue("Engine")
-#define UNIFORM_CAMERA_NAME              HashValue("Camera")
+#define UNIFORM_ENGINE_NAME                 HashValue("Engine")
+#define UNIFORM_CAMERA_NAME                 HashValue("Camera")
 
 
 class CALL_API CGfxRenderer
