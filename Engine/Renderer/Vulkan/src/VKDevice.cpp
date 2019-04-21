3dfe44f01df14e358dce1311c2f8a5b16ddb5609
LiangYue <LiangYue1981816@Hotmail.com>
2019-04-22 00:07:30 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VKDevice.cpp
+++ Engine/Renderer/Vulkan/src/VKDevice.cpp
@@ -51,7 +51,7 @@ bool CVKDevice::SelectPhysicalDevices(eastl::vector<VkPhysicalDevice>& devices, 
 {
 	uint32_t familyIndex = UINT32_MAX;
 
-	for (uint32_t index = 0; index < devices.size(); index++) {
+	for (int index = 0; index < devices.size(); index++) {
 		if (CheckPhysicalDeviceCapabilities(devices[index]) == false) continue;
 		if (CheckPhysicalDeviceExtensionProperties(devices[index]) == false) continue;
 		if (CheckPhysicalDeviceQueueFamilyProperties(devices[index], familyIndex) == false) continue;
