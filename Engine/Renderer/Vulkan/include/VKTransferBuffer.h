583c6bc7dcbe1af9476a1db77aba6f118e3452c3
LiangYue <LiangYue1981816@Hotmail.com>
2019-04-27 21:03:01 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/include/VKTransferBuffer.h
+++ Engine/Renderer/Vulkan/include/VKTransferBuffer.h
@@ -19,7 +19,6 @@ private:
 
 private:
 	bool TransferBufferData(CVKBuffer* pDstBuffer, VkAccessFlags dstAccessFlags, VkPipelineStageFlags dstPipelineStageFlags, size_t offset, size_t size, const void* data);
-
 	bool TransferTexture2DData(CVKTexture* pDstTexture, VkImageLayout dstImageLayout, int level, int xoffset, int yoffset, int width, int height, uint32_t size, const void* data);
 	bool TransferTexture2DArrayData(CVKTexture* pDstTexture, VkImageLayout dstImageLayout, int layer, int level, int xoffset, int yoffset, int width, int height, uint32_t size, const void* data);
 	bool TransferTextureCubemapData(CVKTexture* pDstTexture, VkImageLayout dstImageLayout, int face, int level, int xoffset, int yoffset, int width, int height, uint32_t size, const void* data);
