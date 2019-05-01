061de8f369cbe6a8e5b56dcfca69ec2fc24e463f
LiangYue <LiangYue1981816@Hotmail.com>
2019-05-01 16:32:30 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/include/VkDescriptorLayout.h
+++ Engine/Renderer/Vulkan/include/VkDescriptorLayout.h
@@ -4,5 +4,39 @@
 
 class CVKDescriptorLayout : public CGfxDescriptorLayout
 {
+	friend class CVKDescriptorLayoutManager;
 
+
+private:
+	CVKDescriptorLayout(CVKDevice* pDevice, uint32_t set);
+	virtual ~CVKDescriptorLayout(void);
+	virtual void Release(void);
+
+
+public:
+	bool Create(void);
+	void Destroy(void);
+
+public:
+	bool SetUniformBlockBinding(uint32_t name, uint32_t binding);
+	bool SetSampledImageBinding(uint32_t name, uint32_t binding);
+
+public:
+	uint32_t GetSetIndex(void) const;
+	uint32_t GetUniformBlockBinding(uint32_t name) const;
+	uint32_t GetSampledImageBinding(uint32_t name) const;
+
+public:
+	bool IsUniformBlockValid(uint32_t name) const;
+	bool IsSampledImageValid(uint32_t name) const;
+	bool IsCompatible(const CGfxDescriptorLayoutPtr ptrLayout) const;
+
+
+private:
+	uint32_t m_set;
+	eastl::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> m_uniformBlockBindings; // [name, binding]
+	eastl::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> m_sampledImageBindings; // [name, binding]
+
+private:
+	CVKDevice* m_pDevice;
 };
--- Engine/Renderer/Vulkan/src/VkDescriptorLayout.cpp
+++ Engine/Renderer/Vulkan/src/VkDescriptorLayout.cpp
@@ -1,3 +1,70 @@
 #include "VKRenderer.h"
 
 
+CVKDescriptorLayout::CVKDescriptorLayout(CVKDevice* pDevice, uint32_t set)
+	: CGfxDescriptorLayout(set)
+	, m_pDevice(pDevice)
+	, m_set(set)
+{
+
+}
+
+CVKDescriptorLayout::~CVKDescriptorLayout(void)
+{
+
+}
+
+void CVKDescriptorLayout::Release(void)
+{
+
+}
+
+bool CVKDescriptorLayout::Create(void)
+{
+	return true;
+}
+
+void CVKDescriptorLayout::Destroy(void)
+{
+
+}
+
+bool CVKDescriptorLayout::SetUniformBlockBinding(uint32_t name, uint32_t binding)
+{
+	return true;
+}
+
+bool CVKDescriptorLayout::SetSampledImageBinding(uint32_t name, uint32_t binding)
+{
+	return true;
+}
+
+uint32_t CVKDescriptorLayout::GetSetIndex(void) const
+{
+	return m_set;
+}
+
+uint32_t CVKDescriptorLayout::GetUniformBlockBinding(uint32_t name) const
+{
+	return 0;
+}
+
+uint32_t CVKDescriptorLayout::GetSampledImageBinding(uint32_t name) const
+{
+	return 0;
+}
+
+bool CVKDescriptorLayout::IsUniformBlockValid(uint32_t name) const
+{
+	return true;
+}
+
+bool CVKDescriptorLayout::IsSampledImageValid(uint32_t name) const
+{
+	return true;
+}
+
+bool CVKDescriptorLayout::IsCompatible(const CGfxDescriptorLayoutPtr ptrLayout) const
+{
+	return true;
+}
