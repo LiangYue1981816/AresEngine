1523ef5431e86fd154d42938a51b88be35c2217e
LiangYue <LiangYue1981816@Hotmail.com>
2019-05-01 14:48:09 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VKBuffer.cpp
+++ Engine/Renderer/Vulkan/src/VKBuffer.cpp
@@ -6,6 +6,7 @@ CVKBuffer::CVKBuffer(CVKDevice* pDevice, VkDeviceSize size, VkBufferUsageFlags b
 
 	, m_vkBuffer(VK_NULL_HANDLE)
 	, m_vkBufferUsageFlags(bufferUsageFlags)
+
 	, m_pMemory(nullptr)
 {
 	VkBufferCreateInfo createInfo = {};
@@ -13,22 +14,29 @@ CVKBuffer::CVKBuffer(CVKDevice* pDevice, VkDeviceSize size, VkBufferUsageFlags b
 	createInfo.pNext = nullptr;
 	createInfo.flags = 0;
 	createInfo.size = size;
-	createInfo.usage = m_vkBufferUsageFlags;
+	createInfo.usage = bufferUsageFlags;
 	createInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 	createInfo.queueFamilyIndexCount = 0;
 	createInfo.pQueueFamilyIndices = nullptr;
-	vkCreateBuffer(m_pDevice->GetDevice(), &createInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkBuffer);
+	CALL_VK_FUNCTION_RETURN(vkCreateBuffer(m_pDevice->GetDevice(), &createInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkBuffer));
 
 	VkMemoryRequirements requirements;
 	vkGetBufferMemoryRequirements(m_pDevice->GetDevice(), m_vkBuffer, &requirements);
-	m_pMemory = m_pDevice->GetMemoryManager()->AllocMemory(requirements.size, requirements.alignment, requirements.memoryTypeBits, memoryPropertyFlags);
-	m_pMemory->BindBuffer(m_vkBuffer);
+
+	if (m_pMemory = m_pDevice->GetMemoryManager()->AllocMemory(requirements.size, requirements.alignment, requirements.memoryTypeBits, memoryPropertyFlags)) {
+		m_pMemory->BindBuffer(m_vkBuffer);
+	}
 }
 
 CVKBuffer::~CVKBuffer(void)
 {
-	vkDestroyBuffer(m_pDevice->GetDevice(), m_vkBuffer, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks());
-	m_pDevice->GetMemoryManager()->FreeMemory(m_pMemory);
+	if (m_vkBuffer) {
+		vkDestroyBuffer(m_pDevice->GetDevice(), m_vkBuffer, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks());
+	}
+
+	if (m_pMemory) {
+		m_pDevice->GetMemoryManager()->FreeMemory(m_pMemory);
+	}
 }
 
 void CVKBuffer::Release(void)
@@ -43,42 +51,57 @@ VkBuffer CVKBuffer::GetBuffer(void) const
 
 VkDeviceSize CVKBuffer::GetSize(void) const
 {
-	return m_pMemory->GetSize();
+	if (m_pMemory) {
+		return m_pMemory->GetSize();
+	}
+	else {
+		return 0;
+	}
 }
 
 bool CVKBuffer::BufferData(size_t offset, size_t size, const void* data)
 {
-	if (m_pMemory->IsHostVisible()) {
-		CALL_BOOL_FUNCTION_RETURN_BOOL(m_pMemory->BeginMap(offset, size));
-		CALL_BOOL_FUNCTION_RETURN_BOOL(m_pMemory->CopyData(0, size, data));
-		CALL_BOOL_FUNCTION_RETURN_BOOL(m_pMemory->EndMap());
-		return true;
+	if (m_pMemory) {
+		if (m_pMemory->IsHostVisible()) {
+			CALL_BOOL_FUNCTION_RETURN_BOOL(m_pMemory->BeginMap(offset, size));
+			CALL_BOOL_FUNCTION_RETURN_BOOL(m_pMemory->CopyData(0, size, data));
+			CALL_BOOL_FUNCTION_RETURN_BOOL(m_pMemory->EndMap());
+			return true;
+		}
+		else {
+			VkAccessFlags dstAccessFlags = 0;
+			VkPipelineStageFlags dstPipelineStageFlags = 0;
+
+			if (m_vkBufferUsageFlags & VK_BUFFER_USAGE_INDEX_BUFFER_BIT) {
+				dstAccessFlags |= VK_ACCESS_INDEX_READ_BIT;
+				dstPipelineStageFlags |= VK_PIPELINE_STAGE_VERTEX_INPUT_BIT;
+			}
+
+			if (m_vkBufferUsageFlags & VK_BUFFER_USAGE_VERTEX_BUFFER_BIT) {
+				dstAccessFlags |= VK_ACCESS_VERTEX_ATTRIBUTE_READ_BIT;
+				dstPipelineStageFlags |= VK_PIPELINE_STAGE_VERTEX_INPUT_BIT;
+			}
+
+			if (m_vkBufferUsageFlags & VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT) {
+				dstAccessFlags |= VK_ACCESS_UNIFORM_READ_BIT;
+				dstPipelineStageFlags |= VK_PIPELINE_STAGE_VERTEX_SHADER_BIT | VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
+			}
+
+			return m_pDevice->GetTransferBufferManager()->TransferBufferData(this, dstAccessFlags, dstPipelineStageFlags, offset, size, data);
+		}
 	}
 	else {
-		VkAccessFlags dstAccessFlags = 0;
-		VkPipelineStageFlags dstPipelineStageFlags = 0;
-
-		if (m_vkBufferUsageFlags & VK_BUFFER_USAGE_INDEX_BUFFER_BIT) {
-			dstAccessFlags |= VK_ACCESS_INDEX_READ_BIT;
-			dstPipelineStageFlags |= VK_PIPELINE_STAGE_VERTEX_INPUT_BIT;
-		}
-
-		if (m_vkBufferUsageFlags & VK_BUFFER_USAGE_VERTEX_BUFFER_BIT) {
-			dstAccessFlags |= VK_ACCESS_VERTEX_ATTRIBUTE_READ_BIT;
-			dstPipelineStageFlags |= VK_PIPELINE_STAGE_VERTEX_INPUT_BIT;
-		}
-
-		if (m_vkBufferUsageFlags & VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT) {
-			dstAccessFlags |= VK_ACCESS_UNIFORM_READ_BIT;
-			dstPipelineStageFlags |= VK_PIPELINE_STAGE_VERTEX_SHADER_BIT | VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
-		}
-
-		return m_pDevice->GetTransferBufferManager()->TransferBufferData(this, dstAccessFlags, dstPipelineStageFlags, offset, size, data);
+		return false;
 	}
 }
 
 bool CVKBuffer::PipelineBarrier(VkCommandBuffer vkCommandBuffer, VkAccessFlags srcAccessFlags, VkAccessFlags dstAccessFlags, VkPipelineStageFlags srcPipelineStageFlags, VkPipelineStageFlags dstPipelineStageFlags, VkDeviceSize offset, VkDeviceSize size)
 {
-	CALL_VK_FUNCTION_RETURN_BOOL(vkCmdBufferMemoryBarrier(vkCommandBuffer, m_vkBuffer, srcAccessFlags, dstAccessFlags, srcPipelineStageFlags, dstPipelineStageFlags, offset, size));
-	return true;
+	if (m_vkBuffer) {
+		CALL_VK_FUNCTION_RETURN_BOOL(vkCmdBufferMemoryBarrier(vkCommandBuffer, m_vkBuffer, srcAccessFlags, dstAccessFlags, srcPipelineStageFlags, dstPipelineStageFlags, offset, size));
+		return true;
+	}
+	else {
+		return false;
+	}
 }
