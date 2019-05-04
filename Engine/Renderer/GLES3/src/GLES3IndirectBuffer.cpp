1e754dcad19038456462eff5fbf7ad5e8abe0b69
LiangYue <LiangYue1981816@Hotmail.com>
2019-05-04 14:34:35 +0800

    - 调整代码

--- Engine/Renderer/GLES3/src/GLES3IndirectBuffer.cpp
+++ Engine/Renderer/GLES3/src/GLES3IndirectBuffer.cpp
@@ -14,6 +14,11 @@ CGLES3IndirectBuffer::~CGLES3IndirectBuffer(void)
 	CGfxProfiler::DecIndirectBufferSize(m_ptrBuffer->GetSize());
 }
 
+uint32_t CGLES3IndirectBuffer::GetSize(void) const
+{
+	return m_ptrBuffer->GetSize();
+}
+
 uint32_t CGLES3IndirectBuffer::GetDrawCommandCount(void) const
 {
 	return m_draws.size();
@@ -22,11 +27,6 @@ uint32_t CGLES3IndirectBuffer::GetDrawCommandCount(void) const
 uint32_t CGLES3IndirectBuffer::GetDrawCommandOffset(int indexDraw) const
 {
 	return indexDraw * sizeof(DrawCommand);
-}
-
-uint32_t CGLES3IndirectBuffer::GetSize(void) const
-{
-	return m_ptrBuffer->GetSize();
 }
 
 bool CGLES3IndirectBuffer::BufferData(int indexDraw, int baseVertex, int firstIndex, int indexCount, int instanceCount)
