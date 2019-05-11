6fd62184109d5409e0d95fb2257b0901513b6606
LiangYue <LiangYue1981816@Hotmail.com>
2019-05-11 16:33:32 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VKIndirectBuffer.cpp
+++ Engine/Renderer/Vulkan/src/VKIndirectBuffer.cpp
@@ -7,12 +7,10 @@ CVKIndirectBuffer::CVKIndirectBuffer(CVKDevice* pDevice, int numDrawCommands)
 	, m_pBuffer(nullptr)
 
 	, m_draws(numDrawCommands)
-	, m_size(numDrawCommands * sizeof(DrawCommand))
-	, m_offset(0)
 {
 	ASSERT(m_pDevice);
 
-	m_pBuffer = new CVKBuffer(pDevice, CGfxSwapChain::SWAPCHAIN_FRAME_COUNT * m_size, VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+	m_pBuffer = new CVKBuffer(pDevice, numDrawCommands * sizeof(DrawCommand), VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
 	CGfxProfiler::IncIndirectBufferSize(m_pBuffer->GetSize());
 }
 
@@ -39,12 +37,7 @@ uint32_t CVKIndirectBuffer::GetDrawCommandOffset(int indexDraw) const
 
 uint32_t CVKIndirectBuffer::GetSize(void) const
 {
-	return m_size;
-}
-
-uint32_t CVKIndirectBuffer::GetOffset(void) const
-{
-	return m_offset;
+	return m_pBuffer->GetSize();
 }
 
 bool CVKIndirectBuffer::BufferData(int indexDraw, int baseVertex, int firstIndex, int indexCount, int instanceCount)
@@ -65,6 +58,5 @@ bool CVKIndirectBuffer::BufferData(int indexDraw, int baseVertex, int firstIndex
 	m_draws[indexDraw].indexCount = indexCount;
 	m_draws[indexDraw].instanceCount = instanceCount;
 
-	m_offset = VKRenderer()->GetSwapChain()->GetFrameIndex() * m_size;
-	return m_pBuffer->BufferData(m_offset + indexDraw * sizeof(DrawCommand), sizeof(m_draws[indexDraw]), &m_draws[indexDraw]);
+	return m_pBuffer->BufferData(indexDraw * sizeof(DrawCommand), sizeof(m_draws[indexDraw]), &m_draws[indexDraw]);
 }
