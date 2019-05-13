cb3fbf5e5987e442c64578a44c81f0ef68be3a92
LiangYue <LiangYue1981816@Hotmail.com>
2019-05-13 11:37:21 +0800

    - 调整代码

--- Engine/Renderer/GLES3/src/GLES3DescriptorSetManager.cpp
+++ Engine/Renderer/GLES3/src/GLES3DescriptorSetManager.cpp
@@ -39,14 +39,15 @@ CGLES3DescriptorSet* CGLES3DescriptorSetManager::Create(const CGfxPipelineGraphi
 			if (pSubpassInformation->inputAttachments.size()) {
 				if (m_pInputAttachmentDescriptorSets[(CGLES3FrameBuffer*)pFrameBuffer][(SubpassInformation*)pSubpassInformation][(CGLES3PipelineGraphics*)pPipelineGraphics] == nullptr) {
 					CGLES3DescriptorSet* pDescriptorSet = new CGLES3DescriptorSet(this, pPipelineGraphics->GetDescriptorLayout(DESCRIPTOR_SET_INPUTATTACHMENT));
-					m_pInputAttachmentDescriptorSets[(CGLES3FrameBuffer*)pFrameBuffer][(SubpassInformation*)pSubpassInformation][(CGLES3PipelineGraphics*)pPipelineGraphics] = pDescriptorSet;
-
-					for (const auto& itInputAttachment : pSubpassInformation->inputAttachments) {
-						pDescriptorSet->SetTextureInputAttachment(
-							((CGLES3PipelineGraphics*)pPipelineGraphics)->GetInputAttachmentName(itInputAttachment.first),
-							((CGLES3FrameBuffer*)pFrameBuffer)->GetAttachmentTexture(itInputAttachment.first),
-							GLES3Renderer()->CreateSampler(GFX_FILTER_NEAREST, GFX_FILTER_NEAREST, GFX_SAMPLER_MIPMAP_MODE_NEAREST, GFX_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE));
+					{
+						for (const auto& itInputAttachment : pSubpassInformation->inputAttachments) {
+							pDescriptorSet->SetTextureInputAttachment(
+								((CGLES3PipelineGraphics*)pPipelineGraphics)->GetInputAttachmentName(itInputAttachment.first),
+								((CGLES3FrameBuffer*)pFrameBuffer)->GetAttachmentTexture(itInputAttachment.first),
+								GLES3Renderer()->CreateSampler(GFX_FILTER_NEAREST, GFX_FILTER_NEAREST, GFX_SAMPLER_MIPMAP_MODE_NEAREST, GFX_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE));
+						}
 					}
+					m_pInputAttachmentDescriptorSets[(CGLES3FrameBuffer*)pFrameBuffer][(SubpassInformation*)pSubpassInformation][(CGLES3PipelineGraphics*)pPipelineGraphics] = pDescriptorSet;
 				}
 
 				return m_pInputAttachmentDescriptorSets[(CGLES3FrameBuffer*)pFrameBuffer][(SubpassInformation*)pSubpassInformation][(CGLES3PipelineGraphics*)pPipelineGraphics];
