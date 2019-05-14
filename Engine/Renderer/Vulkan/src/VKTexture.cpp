3325c47252c10e0a1e93b580b312d45fc0f2ec05
LiangYue <LiangYue1981816@Hotmail.com>
2019-05-15 02:22:00 +0800

    - 修正创建Texture Bug

--- Engine/Renderer/Vulkan/src/VKTexture.cpp
+++ Engine/Renderer/Vulkan/src/VKTexture.cpp
@@ -167,17 +167,6 @@ bool CVKTexture::Create(GfxTextureType type, GfxPixelFormat format, int width, i
 			}
 			CALL_VK_FUNCTION_BREAK(vkCreateImage(m_pDevice->GetDevice(), &imageCreateInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkImage));
 
-			VkImageViewCreateInfo viewCreateInfo = {};
-			viewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-			viewCreateInfo.pNext = nullptr;
-			viewCreateInfo.flags = 0;
-			viewCreateInfo.image = m_vkImage;
-			viewCreateInfo.viewType = CVKHelper::TranslateImageViewType(type);
-			viewCreateInfo.format = (VkFormat)format;
-			viewCreateInfo.components = CVKHelper::GetFormatComponentMapping((VkFormat)format);
-			viewCreateInfo.subresourceRange = { imageAspectFlags, 0, (uint32_t)levels, 0, (uint32_t)layers };
-			CALL_VK_FUNCTION_BREAK(vkCreateImageView(m_pDevice->GetDevice(), &viewCreateInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkImageView));
-
 			VkMemoryPropertyFlags memoryPropertyFlags =
 				imageTiling == VK_IMAGE_TILING_LINEAR ?
 				VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT :
@@ -191,6 +180,17 @@ bool CVKTexture::Create(GfxTextureType type, GfxPixelFormat format, int width, i
 			m_pMemory = m_pDevice->GetMemoryManager()->AllocMemory(requirements.size, requirements.alignment, requirements.memoryTypeBits, memoryPropertyFlags);
 			if (m_pMemory == nullptr) { ASSERT(false); break; }
 			if (m_pMemory->BindImage(m_vkImage) == false) { ASSERT(false); break; }
+
+			VkImageViewCreateInfo viewCreateInfo = {};
+			viewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			viewCreateInfo.pNext = nullptr;
+			viewCreateInfo.flags = 0;
+			viewCreateInfo.image = m_vkImage;
+			viewCreateInfo.viewType = CVKHelper::TranslateImageViewType(type);
+			viewCreateInfo.format = (VkFormat)format;
+			viewCreateInfo.components = CVKHelper::GetFormatComponentMapping((VkFormat)format);
+			viewCreateInfo.subresourceRange = { imageAspectFlags, 0, (uint32_t)levels, 0, (uint32_t)layers };
+			CALL_VK_FUNCTION_BREAK(vkCreateImageView(m_pDevice->GetDevice(), &viewCreateInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkImageView));
 
 			return true;
 		} while (false);
