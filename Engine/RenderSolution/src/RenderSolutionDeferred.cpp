10811558d8824b876e85d970f084d7977aa67cb2
LiangYue <LiangYue1981816@Hotmail.com>
2019-06-05 19:08:04 +0800

    + 添加文件

---
+++ Engine/RenderSolution/src/RenderSolutionDeferred.cpp
@@ -0,0 +1,52 @@
+#include "EngineHeader.h"
+
+
+CRenderSolutionDeferred::CRenderSolutionDeferred(void)
+{
+
+}
+
+CRenderSolutionDeferred::~CRenderSolutionDeferred(void)
+{
+	Destroy();
+}
+
+void CRenderSolutionDeferred::CreateFrameBuffer(void)
+{
+
+}
+
+void CRenderSolutionDeferred::DestroyFrameBuffer(void)
+{
+
+}
+
+void CRenderSolutionDeferred::CreateFrameBufferMSAA(int samples)
+{
+
+}
+
+void CRenderSolutionDeferred::DestroyFrameBufferMSAA(void)
+{
+
+}
+
+void CRenderSolutionDeferred::Create(int samples)
+{
+
+}
+
+void CRenderSolutionDeferred::Destroy(void)
+{
+
+}
+
+void CRenderSolutionDeferred::UpdateCamera(int indexQueue)
+{
+
+}
+
+void CRenderSolutionDeferred::Render(int indexQueue)
+{
+
+}
