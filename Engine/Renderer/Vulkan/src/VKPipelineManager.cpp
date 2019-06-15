082bb203d9f4beab31d9a3aac85a7a51a2764cdc
LiangYue <LiangYue1981816@Hotmail.com>
2019-06-15 11:52:05 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VKPipelineManager.cpp
+++ Engine/Renderer/Vulkan/src/VKPipelineManager.cpp
@@ -1,15 +1,51 @@
 #include "VKRenderer.h"
 
 
+static void LoadPipelineCache(const char* szFileName, eastl::vector<uint8_t>& cacheData)
+{
+	if (FILE* pFile = fopen(szFileName, "rb")) {
+		cacheData.resize(fsize(pFile));
+		fread(cacheData.data(), 1, cacheData.size(), pFile);
+		fclose(pFile);
+	}
+}
+
+static void SavePipelineCache(const char* szFileName, eastl::vector<uint8_t>& cacheData)
+{
+	if (FILE* pFile = fopen(szFileName, "wb")) {
+		fwrite(cacheData.data(), 1, cacheData.size(), pFile);
+		fclose(pFile);
+	}
+}
+
+
 CVKPipelineComputeManager::CVKPipelineComputeManager(CVKDevice* pDevice)
 	: m_pDevice(pDevice)
 	, m_vkPipelineCache(VK_NULL_HANDLE)
 {
+	eastl::vector<uint8_t> cacheData;
+	LoadPipelineCache("./ComputeCache.data", cacheData);
 
+	VkPipelineCacheCreateInfo cacheCreateInfo = {};
+	cacheCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO;
+	cacheCreateInfo.pNext = nullptr;
+	cacheCreateInfo.flags = 0;
+	cacheCreateInfo.initialDataSize = cacheData.size();
+	cacheCreateInfo.pInitialData = cacheData.data();
+	vkCreatePipelineCache(m_pDevice->GetDevice(), &cacheCreateInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkPipelineCache);
 }
 
 CVKPipelineComputeManager::~CVKPipelineComputeManager(void)
 {
+	size_t size = 0;
+	vkGetPipelineCacheData(m_pDevice->GetDevice(), m_vkPipelineCache, &size, nullptr);
+
+	eastl::vector<uint8_t> cacheData(size);
+	vkGetPipelineCacheData(m_pDevice->GetDevice(), m_vkPipelineCache, &size, cacheData.data());
+	vkDestroyPipelineCache(m_pDevice->GetDevice(), m_vkPipelineCache, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks());
+
+	SavePipelineCache("./ComputeCache.data", cacheData);
+
 	for (const auto& itPipeline : m_pPipelines) {
 		delete itPipeline.second;
 	}
@@ -22,7 +58,7 @@ CVKPipelineCompute* CVKPipelineComputeManager::Create(const CGfxShader* pCompute
 		uint32_t name = HashValueFormat("%x", pComputeShader->GetName());
 
 		if (m_pPipelines[name] == nullptr) {
-			m_pPipelines[name] = new CVKPipelineCompute(m_pDevice, VK_NULL_HANDLE, name);
+			m_pPipelines[name] = new CVKPipelineCompute(m_pDevice, m_vkPipelineCache, name);
 			m_pPipelines[name]->Create(pComputeShader);
 		}
 
@@ -35,11 +71,29 @@ CVKPipelineGraphicsManager::CVKPipelineGraphicsManager(CVKDevice* pDevice)
 	: m_pDevice(pDevice)
 	, m_vkPipelineCache(VK_NULL_HANDLE)
 {
+	eastl::vector<uint8_t> cacheData;
+	LoadPipelineCache("./GraphicsCache.data", cacheData);
 
+	VkPipelineCacheCreateInfo cacheCreateInfo = {};
+	cacheCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO;
+	cacheCreateInfo.pNext = nullptr;
+	cacheCreateInfo.flags = 0;
+	cacheCreateInfo.initialDataSize = cacheData.size();
+	cacheCreateInfo.pInitialData = cacheData.data();
+	vkCreatePipelineCache(m_pDevice->GetDevice(), &cacheCreateInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkPipelineCache);
 }
 
 CVKPipelineGraphicsManager::~CVKPipelineGraphicsManager(void)
 {
+	size_t size = 0;
+	vkGetPipelineCacheData(m_pDevice->GetDevice(), m_vkPipelineCache, &size, nullptr);
+
+	eastl::vector<uint8_t> cacheData(size);
+	vkGetPipelineCacheData(m_pDevice->GetDevice(), m_vkPipelineCache, &size, cacheData.data());
+	vkDestroyPipelineCache(m_pDevice->GetDevice(), m_vkPipelineCache, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks());
+
+	SavePipelineCache("./GraphicsCache.data", cacheData);
+
 	for (const auto& itPipeline : m_pPipelines) {
 		delete itPipeline.second;
 	}
@@ -52,7 +106,7 @@ CVKPipelineGraphics* CVKPipelineGraphicsManager::Create(const CGfxRenderPass* pR
 		uint32_t name = HashValueFormat("%p_%x_%x_%x_%x_%x_%x", pRenderPass, pVertexShader->GetName(), pFragmentShader->GetName(), HashValue((uint8_t*)& state, sizeof(state)), indexSubpass, vertexBinding, instanceBinding);
 
 		if (m_pPipelines[name] == nullptr) {
-			m_pPipelines[name] = new CVKPipelineGraphics(m_pDevice, VK_NULL_HANDLE, name);
+			m_pPipelines[name] = new CVKPipelineGraphics(m_pDevice, m_vkPipelineCache, name);
 			m_pPipelines[name]->Create(pRenderPass, pVertexShader, pFragmentShader, state, indexSubpass, vertexBinding, instanceBinding);
 		}
 
