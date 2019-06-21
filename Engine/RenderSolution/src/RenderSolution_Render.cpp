8b2471e203182e16070f1edbc0cadfbcf8b61268
LiangYue <LiangYue1981816@Hotmail.com>
2019-06-21 21:16:45 +0800

    + 添加调试代码

--- Engine/RenderSolution/src/RenderSolution_Render.cpp
+++ Engine/RenderSolution/src/RenderSolution_Render.cpp
@@ -55,7 +55,7 @@ void CRenderSolution::RenderForward(int indexQueue)
 	GfxRenderer()->AcquireNextFrame();
 	{
 		m_pPassShadow->Render(indexQueue);
-		m_pPassForwardLighting->Render(indexQueue, m_bEnableMSAA);
+//		m_pPassForwardLighting->Render(indexQueue, m_bEnableMSAA);
 	}
 	GfxRenderer()->Present();
 }
