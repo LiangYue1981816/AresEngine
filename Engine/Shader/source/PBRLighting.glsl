5ea8d404ce9bed8203cad8189836189134049fcf
LiangYue <LiangYue1981816@Hotmail.com>
2019-08-19 00:50:08 +0800

    - 调整代码

--- Engine/Shader/source/PBRLighting.glsl
+++ Engine/Shader/source/PBRLighting.glsl
@@ -146,7 +146,7 @@ if (factor < mainShadowLevelFactor.w) finalLighting = vec3(1.0, 1.0, 0.0) * vec3
 if (factor < mainShadowLevelFactor.z) finalLighting = vec3(0.0, 1.0, 1.0) * vec3(shadow);
 if (factor < mainShadowLevelFactor.y) finalLighting = vec3(0.0, 1.0, 0.0) * vec3(shadow);
 if (factor < mainShadowLevelFactor.x) finalLighting = vec3(1.0, 0.0, 0.0) * vec3(shadow);
-finalLighting = vec3(shadow);
+//finalLighting = vec3(shadow);
 
 	outFragColor.rgb = finalLighting;
 	outFragColor.a = 1.0;
