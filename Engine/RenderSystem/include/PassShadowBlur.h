ddb6b6bb9a49563f2f99caa4ee0643634535233f
LiangYue <LiangYue1981816@Hotmail.com>
2019-08-23 21:40:49 +0800

    - 调整代码

---
+++ Engine/RenderSystem/include/PassShadowBlur.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "RenderSystem.h"
+
+
+class CALL_API CPassShadow
+{
+	friend class CCamera;
+	friend class CRenderSystem;
+
+
+private:
+	CPassShadow(CCamera* pCamera, CRenderSystem* pRenderSystem);
+	virtual ~CPassShadow(void);
+
+
+private:
+	static void Create(GfxPixelFormat shadowPixelFormat, GfxPixelFormat depthPixelFormat);
+	static void Destroy(void);
+
+public:
+	void CreateFrameBuffer(CGfxRenderTexturePtr ptrShadowTexture, CGfxRenderTexturePtr ptrDepthStencilTexture);
+
+private:
+	const CGfxSemaphore* Render(CTaskGraph& taskGraph, const CGfxSemaphore* pWaitSemaphore);
+
+
+private:
+	float m_splitFactors[5];
+	CGfxCamera* m_pShadowCamera[4];
+	CGfxRenderQueue* m_pShadowRenderQueue[4];
+
+private:
+	CGfxFrameBufferPtr m_ptrFrameBuffer;
+	CGfxRenderTexturePtr m_ptrShadowTexture;
+	CGfxRenderTexturePtr m_ptrDepthStencilTexture;
+
+private:
+	CGfxUniformCamera* m_pShadowCameraUniform[4];
+	CGfxDescriptorSetPtr m_ptrDescriptorSetPass[4];
+	CGfxCommandBufferPtr m_ptrMainCommandBuffer[CGfxSwapChain::SWAPCHAIN_FRAME_COUNT];
+
+private:
+	CCamera* m_pCamera;
+	CRenderSystem* m_pRenderSystem;
+};
