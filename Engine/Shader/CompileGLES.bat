46c0f82371c7b917c5a0525ee70e7e8cd8e6ab12
LiangYue <LiangYue1981816@Hotmail.com>
2019-08-25 15:58:16 +0800

    - 调整代码

--- Engine/Shader/CompileGLES.bat
+++
@@ -1,9 +0,0 @@
-for %%i in (.\source\*.glsl) do (
-	glslc -fshader-stage=vertex -DGLES -DVERTEX_SHADER -I".\source\inc" %%i -o .\bin\%%~ni.vert
-	glslc -fshader-stage=fragment -DGLES -DFRAGMENT_SHADER -I".\source\inc" %%i -o .\bin\%%~ni.frag
-	
-	spirv-cross.exe --es --version 310 .\bin\%%~ni.vert --output .\debug\%%~ni.vert
-	spirv-cross.exe --es --version 310 .\bin\%%~ni.frag --output .\debug\%%~ni.frag
-)
-
-pause
