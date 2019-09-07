574b3d60c6b66bb7c0229e9df635fa38c86edc32
LiangYue <LiangYue1981816@Hotmail.com>
2019-09-08 00:27:22 +0800

    - 调整代码

--- Engine/Shader/source/SSAO.glsl
+++ Engine/Shader/source/SSAO.glsl
@@ -24,6 +24,7 @@ void main()
 #ifdef FRAGMENT_SHADER
 precision mediump float;
 #include "engine.inc"
+#include "common.inc"
 
 
 // Input
@@ -39,6 +40,37 @@ USE_COLOR_TEXTURE_UNIFORM;
 
 void main()
 {
-	outFragColor = texture(texColor, inTexcoord);
+	highp vec3 sample_sphere[16] = vec3[] (
+		vec3( 0.5381, 0.1856,-0.4319), vec3( 0.1379, 0.2486, 0.4430),
+		vec3( 0.3371, 0.5679,-0.0057), vec3(-0.6999,-0.0451,-0.0019),
+		vec3( 0.0689,-0.1598,-0.8547), vec3( 0.0560, 0.0069,-0.1843),
+		vec3(-0.0146, 0.1402, 0.0762), vec3( 0.0100,-0.1924,-0.0344),
+		vec3(-0.3577,-0.5301,-0.4358), vec3(-0.3169, 0.1063, 0.0158),
+		vec3( 0.0103,-0.5869, 0.0046), vec3(-0.0897,-0.4940, 0.3287),
+		vec3( 0.7119,-0.0154,-0.0918), vec3(-0.0533, 0.0596,-0.5411),
+		vec3( 0.0352,-0.0631, 0.5460), vec3(-0.4776, 0.2847,-0.0271));
+
+	highp float curDepth = texture(texDepth, inTexcoord).r;
+	highp vec3 curPosition = ScreenToViewPosition(inTexcoord, curDepth, cameraProjectionInverseMatrix);
+
+	highp float radius = 0.1;
+	highp float occlusion = 0.0;
+
+	for (int index = 0; index < 8; index++) {
+		highp vec3 normal = normalize(vec3(PoissonDisk(curPosition, index), 0.0));
+		highp vec3 samplePosition = curPosition + reflect(sample_sphere[index], normal) * radius;
+
+		highp vec4 offset = vec4(samplePosition, 1.0);
+        offset = cameraProjectionMatrix * offset;
+        offset.xy = offset.xy / offset.w;
+        offset.xy = offset.xy * 0.5 + 0.5;
+
+		highp float sampleDepth = texture(texDepth, offset.xy).r;
+		occlusion += step(curDepth, sampleDepth);
+	}
+
+	outFragColor.rgb = vec3(occlusion / 8.0);
+	outFragColor.a = 1.0;
 }
+
 #endif
