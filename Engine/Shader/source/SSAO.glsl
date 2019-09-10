5092755ef50d0803e9dbf9e05261b96f046eb662
LiangYue <LiangYue1981816@Hotmail.com>
2019-09-10 21:21:52 +0800

    - 调整代码

--- Engine/Shader/source/SSAO.glsl
+++ Engine/Shader/source/SSAO.glsl
@@ -15,7 +15,11 @@ layout (location = 0) out mediump vec2 outTexcoord;
 
 void main()
 {
+#ifdef _VULKAN_
 	highp mat4 projectionViewMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0,-0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
+#else
+	highp mat4 projectionViewMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0,  1.0, 0.0, 0.0, 0.0, 0.0,-0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
+#endif
 	highp mat4 worldMatrix = mat4(inInstanceTransformMatrixCol0, inInstanceTransformMatrixCol1, inInstanceTransformMatrixCol2, inInstanceTransformMatrixCol3);
 	gl_Position = projectionViewMatrix * worldMatrix * vec4(inPosition.xyz, 1.0);
 	outTexcoord = inTexcoord0;
