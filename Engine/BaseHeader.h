5ed03160b6348498e6ccd3e24539d9db955e091e
LiangYue <LiangYue1981816@Hotmail.com>
2019-09-13 09:06:27 +0800

    + 添加文件

---
+++ Engine/BaseHeader.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "PreHeader.h"
+
+#include "Memory/include/Allocator.h"
+#include "Base/include/Utils.h"
+#include "Base/include/Lock.h"
+#include "Base/include/Task.h"
+#include "Base/include/TaskPool.h"
+#include "Base/include/TaskGraph.h"
+#include "Base/include/Stream.h"
+#include "Base/include/File.h"
+#include "Base/include/FileManager.h"
+#include "Base/include/SharedPtr.h"
+#include "Base/include/Delegate.h"
