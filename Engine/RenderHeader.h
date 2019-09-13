ddfed752a2a7e66df333ff4859ec97e9b1d02d95
LiangYue <LiangYue1981816@Hotmail.com>
2019-09-13 09:36:03 +0800

    + 添加文件

---
+++ Engine/RenderHeader.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "PreHeader.h"
+#include "BaseHeader.h"
+#include "GfxHeader.h"
+#include "SceneHeader.h"
+
+#include "RenderSystem/include/Camera.h"
+#include "RenderSystem/include/RenderSystem.h"
+#include "RenderSystem/include/PassBlit.h"
+#include "RenderSystem/include/PassPreZ.h"
+#include "RenderSystem/include/PassSSAO.h"
+#include "RenderSystem/include/PassShadow.h"
+#include "RenderSystem/include/PassDefault.h"
+#include "RenderSystem/include/PassForwardLighting.h"
+#include "RenderSystem/include/PassColorGrading.h"
+#include "RenderSystem/include/PassFinal.h"
