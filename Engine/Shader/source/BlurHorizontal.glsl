1b85219ba38647f40bd1af5713271c12250485bb
LiangYue <LiangYue1981816@Hotmail.com>
2019-09-14 08:16:31 +0800

    + 添加材质

---
+++ Engine/Shader/source/BlurHorizontal.glsl
@@ -0,0 +1,59 @@
+#version 310 es
+
+#ifdef VERTEX_SHADER
+precision mediump float;
+#include "engine.inc"
+
+
+// VERTEX_ATTRIBUTE_POSITION;
+// VERTEX_ATTRIBUTE_TEXCOORD0;
+// INSTANCE_ATTRIBUTE_TRANSFORM;
+
+// Output
+layout (location = 0) out mediump vec2 outTexcoord;
+
+
+void main()
+{
+#ifdef _VULKAN_
+	highp mat4 projectionViewMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0,-0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
+#else
+	highp mat4 projectionViewMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0,  1.0, 0.0, 0.0, 0.0, 0.0,-0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
+#endif
+	highp mat4 worldMatrix = mat4(inInstanceTransformMatrixCol0, inInstanceTransformMatrixCol1, inInstanceTransformMatrixCol2, inInstanceTransformMatrixCol3);
+	gl_Position = projectionViewMatrix * worldMatrix * vec4(inPosition.xyz, 1.0);
+	outTexcoord = inTexcoord0;
+}
+#endif
+
+#ifdef FRAGMENT_SHADER
+precision mediump float;
+#include "engine.inc"
+
+
+// Input
+layout (location = 0) in mediump vec2 inTexcoord;
+
+// Output
+layout (location = 0) out mediump vec4 outFragColor;
+
+// Descriptor
+USE_COLOR_TEXTURE_UNIFORM;
+
+
+void main()
+{
+	highp vec3 result = vec3(0.0);
+	highp vec2 texelSize = vec2(1.0) / vec2(textureSize(texColor, 0));
+
+	for (highp float x = -1.0; x <= 1.0; x++) {
+		for (highp float y = -1.0; y <= 1.0; y++) {
+			highp vec2 offset = vec2(x, y) * texelSize;
+			result += texture(texColor, inTexcoord + offset).rgb;
+		}
+	}
+
+	outFragColor.rgb = result / 9.0;
+	outFragColor.a = 1.0;
+}
+#endif
