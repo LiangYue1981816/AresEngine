5c0116dd6ea56e096c44d53d97ee92a447dfb02f
LiangYue <LiangYue1981816@Hotmail.com>
2019-12-07 16:55:03 +0800

    + 添加文件

---
+++ Engine/Renderer/Gfx/include/GfxGPUScene.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "PreHeader.h"
+
+
+class CALL_API CTaskGraph;
+class CALL_API CGfxRenderQueue
+{
+public:
+	CGfxRenderQueue(void);
+	virtual ~CGfxRenderQueue(void);
+
+
+public:
+	void Clear(void);
+	void Begin(void);
+	void Add(int indexThread, const CGfxMaterialPtr ptrMaterial, const CGfxMeshDrawPtr ptrMeshDraw, const uint8_t* pInstanceData, uint32_t size);
+	void End(void);
+
+public:
+	virtual void CmdDraw(CTaskGraph* pTaskGraph, CGfxCommandBufferPtr ptrCommandBuffer, const CGfxDescriptorSetPtr ptrDescriptorSetPass, const uint32_t matPassName, const glm::vec4& scissor, const glm::vec4& viewport, uint32_t mask, bool bIsTransparency);
+	virtual void CmdDrawThread(CGfxCommandBufferPtr ptrCommandBuffer, const CGfxFrameBufferPtr ptrFrameBuffer, const CGfxRenderPassPtr ptrRenderPass, const int indexSubpass, const CGfxDescriptorSetPtr ptrDescriptorSetPass, const CGfxDescriptorSetPtr ptrDescriptorSetInputAttachment, const CGfxPipelineGraphics* pPipeline, const uint32_t matPassName, const glm::vec4& scissor, const glm::vec4& viewport, uint32_t mask);
+
+
+private:
+	eastl::unordered_map<CGfxMaterialPtr, eastl::unordered_map<CGfxMeshPtr, eastl::unordered_map<CGfxMeshDrawPtr, eastl::vector<uint8_t>>>> m_materialMeshDrawQueue;
+	eastl::unordered_map<CGfxMaterialPtr, eastl::unordered_map<CGfxMeshPtr, eastl::unordered_map<CGfxMeshDrawPtr, eastl::vector<uint8_t>>>> m_materialMeshDrawQueueThreads[MAX_THREAD_COUNT];
+	eastl::unordered_map<const CGfxPipelineGraphics*, eastl::unordered_map<CGfxMaterialPtr, CGfxMaterialPtr>> m_pipelineMaterialQueue;
+};
