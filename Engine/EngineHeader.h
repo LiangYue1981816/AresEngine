bcdff16fbd85fc64c304ee460b756fc8e7c4252d
LiangYue <LiangYue1981816@Hotmail.com>
2019-12-08 11:08:24 +0800

    - 调整代码

--- Engine/EngineHeader.h
+++ Engine/EngineHeader.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "PreHeader.h"
 #include "BaseHeader.h"
-#include "ConsoleHeader.h"
-#include "GfxHeader.h"
 #include "SceneHeader.h"
 #include "RenderHeader.h"
+#include "ConsoleHeader.h"
 
 #include "Resource/include/ResourceLoader.h"
 #include "Engine/include/Engine.h"
