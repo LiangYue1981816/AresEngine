bf3747a16340d0ade9a4d516e33da591f68aecb3
LiangYue <LiangYue1981816@Hotmail.com>
2019-12-16 15:25:08 +0800

    - 调整代码

--- Engine/Renderer/GLES3/include/GLES3DescriptorLayout.h
+++ Engine/Renderer/GLES3/include/GLES3DescriptorLayout.h
@@ -18,21 +18,21 @@ public:
 	void Destroy(bool bClear = true);
 
 public:
-	void SetUniformBlockBinding(uint32_t name, uint32_t binding);
 	void SetStorageBlockBinding(uint32_t name, uint32_t binding);
+	void SetUniformBlockBinding(uint32_t name, uint32_t binding);
 	void SetSampledImageBinding(uint32_t name, uint32_t binding);
 	void SetInputAttachmentBinding(uint32_t name, uint32_t binding);
 
 public:
 	uint32_t GetSetIndex(void) const;
-	uint32_t GetUniformBlockBinding(uint32_t name) const;
 	uint32_t GetStorageBlockBinding(uint32_t name) const;
+	uint32_t GetUniformBlockBinding(uint32_t name) const;
 	uint32_t GetSampledImageBinding(uint32_t name) const;
 	uint32_t GetInputAttachmentBinding(uint32_t name) const;
 
 public:
-	bool IsUniformBlockValid(uint32_t name) const;
 	bool IsStorageBlockValid(uint32_t name) const;
+	bool IsUniformBlockValid(uint32_t name) const;
 	bool IsSampledImageValid(uint32_t name) const;
 	bool IsInputAttachmentValid(uint32_t name) const;
 	bool IsCompatible(const CGfxDescriptorLayoutPtr ptrLayout) const;
@@ -40,8 +40,8 @@ public:
 
 private:
 	uint32_t m_set;
-	eastl::unordered_map<uint32_t, uint32_t> m_uniformBlockBindings;    // [name, binding]
 	eastl::unordered_map<uint32_t, uint32_t> m_storageBlockBindings;    // [name, binding]
+	eastl::unordered_map<uint32_t, uint32_t> m_uniformBlockBindings;    // [name, binding]
 	eastl::unordered_map<uint32_t, uint32_t> m_sampledImageBindings;    // [name, binding]
 	eastl::unordered_map<uint32_t, uint32_t> m_inputAttachmentBindings; // [name, binding]
 
