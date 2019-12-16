df8d04deb4af74fe98649e129768286550760651
LiangYue <LiangYue1981816@Hotmail.com>
2019-12-16 15:47:31 +0800

    - 调整代码

--- Engine/Renderer/Gfx/include/GfxDescriptorSet.h
+++ Engine/Renderer/Gfx/include/GfxDescriptorSet.h
@@ -4,18 +4,23 @@
 
 typedef struct DescriptorBufferInfo {
 	bool bDirty = false;
+
 	uint32_t offset = 0;
 	uint32_t range = 0;
+
 	CGfxUniformBufferPtr ptrUniformBuffer;
 	CGfxStorageBufferPtr ptrStorageBuffer;
 } DescriptorBufferInfo;
 
 typedef struct DescriptorImageInfo {
 	bool bDirty = false;
+
 	CGfxSampler* pSampler = nullptr;
+
 	CGfxTexture2DPtr ptrTexture2D;
 	CGfxTexture2DArrayPtr ptrTexture2DArray;
 	CGfxTextureCubemapPtr ptrTextureCubemap;
+
 	CGfxRenderTexturePtr ptrRenderTexture;
 	CGfxRenderTexturePtr ptrInputAttachmentTexture;
 } DescriptorImageInfo;
@@ -43,13 +48,16 @@ public:
 	virtual bool SetTexture2D(uint32_t name, const CGfxTexture2DPtr ptrTexture, const CGfxSampler* pSampler) = 0;
 	virtual bool SetTexture2DArray(uint32_t name, const CGfxTexture2DArrayPtr ptrTexture, const CGfxSampler* pSampler) = 0;
 	virtual bool SetTextureCubemap(uint32_t name, const CGfxTextureCubemapPtr ptrTexture, const CGfxSampler* pSampler) = 0;
+
 	virtual bool SetRenderTexture(uint32_t name, const CGfxRenderTexturePtr ptrTexture, const CGfxSampler* pSampler) = 0;
 	virtual bool SetInputAttachmentTexture(uint32_t name, const CGfxRenderTexturePtr ptrTexture, const CGfxSampler* pSampler) = 0;
+
 	virtual bool SetUniformBuffer(uint32_t name, const CGfxUniformBufferPtr ptrUniformBuffer, uint32_t offset, uint32_t range) = 0;
 	virtual bool SetStorageBuffer(uint32_t name, const CGfxStorageBufferPtr ptrStorageBuffer, uint32_t offset, uint32_t range) = 0;
 
 public:
 	virtual const CGfxDescriptorLayoutPtr GetDescriptorLayout(void) const = 0;
+
 	virtual const DescriptorImageInfo* GetDescriptorImageInfo(uint32_t name) const = 0;
 	virtual const DescriptorBufferInfo* GetDescriptorBufferInfo(uint32_t name) const = 0;
 
