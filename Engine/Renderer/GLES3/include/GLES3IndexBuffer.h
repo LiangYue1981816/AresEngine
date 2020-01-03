c9397ae6a515369a19c1231cfde4269d419379a7
LiangYue <LiangYue1981816@Hotmail.com>
2020-01-03 15:04:03 +0800

    - 调整代码

--- Engine/Renderer/GLES3/include/GLES3IndexBuffer.h
+++ Engine/Renderer/GLES3/include/GLES3IndexBuffer.h
@@ -5,6 +5,7 @@
 class CGLES3IndexBuffer : public CGfxIndexBuffer
 {
 	friend class CGLES3Mesh;
+	friend class CGLES3MultiIndexBuffer;
 
 
 private:
@@ -30,4 +31,35 @@ private:
 
 private:
 	CGLES3Buffer* m_pBuffer;
+};
+
+class CGLES3MultiIndexBuffer : public CGfxMultiIndexBuffer
+{
+	friend class CGLES3Mesh;
+
+
+private:
+	CGLES3MultiIndexBuffer(GfxIndexType type, size_t size, bool bDynamic, int count);
+	virtual ~CGLES3MultiIndexBuffer(void);
+	virtual void Release(void);
+
+
+public:
+	void SetBufferIndex(int index);
+
+public:
+	GfxIndexType GetIndexType(void) const;
+	uint32_t GetIndexCount(void) const;
+	uint32_t GetSize(void) const;
+
+public:
+	bool BufferData(size_t offset, size_t size, const void* data);
+
+public:
+	void Bind(void) const;
+
+
+private:
+	int m_index;
+	eastl::vector<CGLES3IndexBuffer*> m_pBuffers;
 };
--- Engine/Renderer/GLES3/src/GLES3IndexBuffer.cpp
+++ Engine/Renderer/GLES3/src/GLES3IndexBuffer.cpp
@@ -50,3 +50,58 @@ void CGLES3IndexBuffer::Bind(void) const
 {
 	m_pBuffer->Bind();
 }
+
+
+CGLES3MultiIndexBuffer::CGLES3MultiIndexBuffer(GfxIndexType type, size_t size, bool bDynamic, int count)
+	: CGfxMultiIndexBuffer(type, size, bDynamic, count)
+	, m_index(0)
+	, m_pBuffers(std::max(1, count))
+{
+	for (int index = 0; index < m_pBuffers.size(); index++) {
+		m_pBuffers[index] = new CGLES3IndexBuffer(type, size, bDynamic);
+	}
+}
+
+CGLES3MultiIndexBuffer::~CGLES3MultiIndexBuffer(void)
+{
+	for (auto& itBuffer : m_pBuffers) {
+		delete itBuffer;
+	}
+}
+
+void CGLES3MultiIndexBuffer::Release(void)
+{
+	delete this;
+}
+
+void CGLES3MultiIndexBuffer::SetBufferIndex(int index)
+{
+	m_index = index;
+	m_index = std::min(m_index, (int)m_pBuffers.size() - 1);
+	m_index = std::max(m_index, 0);
+}
+
+GfxIndexType CGLES3MultiIndexBuffer::GetIndexType(void) const
+{
+	return m_pBuffers[m_index]->GetIndexType();
+}
+
+uint32_t CGLES3MultiIndexBuffer::GetIndexCount(void) const
+{
+	return m_pBuffers[m_index]->GetIndexCount();
+}
+
+uint32_t CGLES3MultiIndexBuffer::GetSize(void) const
+{
+	return m_pBuffers[m_index]->GetSize();
+}
+
+bool CGLES3MultiIndexBuffer::BufferData(size_t offset, size_t size, const void* data)
+{
+	return m_pBuffers[m_index]->BufferData(offset, size, data);
+}
+
+void CGLES3MultiIndexBuffer::Bind(void) const
+{
+	m_pBuffers[m_index]->Bind();
+}
