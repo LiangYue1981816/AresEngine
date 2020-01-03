fad831c543e83a2d8be6dcccaee4961c8d74047e
LiangYue <LiangYue1981816@Hotmail.com>
2020-01-03 16:10:24 +0800

    - GLES3UniformBuffer, 使用动态Buffer

--- Engine/Renderer/GLES3/src/GLES3UniformBuffer.cpp
+++ Engine/Renderer/GLES3/src/GLES3UniformBuffer.cpp
@@ -5,8 +5,14 @@ CGLES3UniformBuffer::CGLES3UniformBuffer(CGLES3UniformBufferManager* pManager, s
 	: CGfxUniformBuffer(size)
 	, m_pManager(pManager)
 	, m_pBuffer(nullptr)
+
+	, m_size(0)
+	, m_offset(0)
 {
-	m_pBuffer = new CGLES3Buffer(GL_UNIFORM_BUFFER, size, true);
+	m_size = size;
+	m_size = ALIGN_BYTE(m_size, 16);
+
+	m_pBuffer = new CGLES3Buffer(GL_UNIFORM_BUFFER, m_size * CGfxSwapChain::SWAPCHAIN_FRAME_COUNT, true);
 	CGfxProfiler::IncUniformBufferSize(m_pBuffer->GetSize());
 }
 
@@ -23,15 +29,21 @@ void CGLES3UniformBuffer::Release(void)
 
 uint32_t CGLES3UniformBuffer::GetSize(void) const
 {
-	return m_pBuffer->GetSize();
+	return m_size;
+}
+
+uint32_t CGLES3UniformBuffer::GetOffset(void) const
+{
+	return m_offset;
 }
 
 bool CGLES3UniformBuffer::BufferData(size_t offset, size_t size, const void* data)
 {
-	return m_pBuffer->BufferData(offset, size, data);
+	m_offset = GLES3Renderer()->GetSwapChain()->GetFrameIndex() * m_size;
+	return m_pBuffer->BufferData(offset, size, data, false);
 }
 
 void CGLES3UniformBuffer::Bind(int binding, int offset, int size) const
 {
-	m_pBuffer->Bind(binding, offset, size);
+	m_pBuffer->Bind(binding, m_offset + offset, size);
 }
