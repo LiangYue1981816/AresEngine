9d0441349249cba97c37aea4a69cbe94a74ddd16
LiangYue <LiangYue1981816@Hotmail.com>
2020-03-02 22:21:47 +0800

    - 调整着色器

---
+++ Engine/Shader/Shader/Debug.glsl
@@ -0,0 +1,42 @@
+#version 310 es
+
+#ifdef VERTEX_SHADER
+precision mediump float;
+#include "engine.inc"
+#include "common.inc"
+
+USE_CAMERA_UNIFORM;
+USE_ENGINE_UNIFORM;
+USE_SCENE_DATA_STORAGE;
+
+// Descriptor
+// ...
+
+void main()
+{
+	highp mat4 worldMatrix = sceneData.data[inInstanceIndex].transformMatrix;
+	highp vec3 worldPosition = (worldMatrix * vec4(inPosition.xyz, 1.0)).xyz;
+
+	gl_Position = cameraProjectionViewMatrix * vec4(worldPosition, 1.0);
+}
+#endif
+
+#ifdef FRAGMENT_SHADER
+precision mediump float;
+#include "engine.inc"
+#include "common.inc"
+
+USE_CAMERA_UNIFORM;
+USE_ENGINE_UNIFORM;
+
+// Output
+layout (location = 0) out mediump vec4 outFragColor;
+
+// Descriptor
+// ...
+
+void main()
+{
+	outFragColor = PackHDR(vec3(1.0, 0.0, 0.0));
+}
+#endif
