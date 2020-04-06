90b95847e5cfb4ca473585247c46f859921aea72
LiangYue <LiangYue1981816@Hotmail.com>
2020-04-06 22:48:24 +0800

    + 添加计算屏幕坐标

--- Engine/Shader/Shader/DeferredLighting_Shading.glsl
+++ Engine/Shader/Shader/DeferredLighting_Shading.glsl
@@ -10,7 +10,7 @@ USE_ENGINE_UNIFORM;
 USE_SCENE_DATA_STORAGE;
 
 // Output
-// ...
+layout (location = 0) out highp vec4 outPosition;
 
 // Descriptor
 // ...
@@ -19,7 +19,7 @@ void main()
 {
 	highp mat4 worldMatrix = sceneData.data[inInstanceIndex].transformMatrix;
 	highp vec3 worldPosition = (worldMatrix * vec4(inPosition.xyz, 1.0)).xyz;
-	gl_Position = cameraProjectionViewMatrix * vec4(worldPosition, 1.0);
+	gl_Position = outPosition = cameraProjectionViewMatrix * vec4(worldPosition, 1.0);
 }
 #endif
 
@@ -35,7 +35,7 @@ USE_DEPTH_TEXTURE_UNIFORM;
 #include "light.inc"
 
 // Input
-// ...
+layout (location = 0) in highp vec4 inPosition;
 
 // Output
 layout (location = 0) out mediump vec4 outFragColor;
@@ -47,11 +47,15 @@ DESCRIPTOR_SET_INPUTATTACHMENT(3, 8) uniform mediump subpassInput texGBuffer2;
 
 void main()
 {
-	vec4 texAlbedo = subpassLoad(texGBuffer0);
-	vec4 texNormal = subpassLoad(texGBuffer1);
-	vec4 texRoughnessMetallicSpecularAO = subpassLoad(texGBuffer2);
-	
-	outFragColor.rgb = texAlbedo.rgb;
+	highp vec4 texCoord = inPosition;
+	texCoord.xy = texCoord.xy / texCoord.w;
+	texCoord.xy = texCoord.xy * 0.5 + 0.5;
+
+	mediump vec4 texAlbedo = subpassLoad(texGBuffer0);
+	mediump vec4 texNormal = subpassLoad(texGBuffer1);
+	mediump vec4 texRoughnessMetallicSpecularAO = subpassLoad(texGBuffer2);
+
+	outFragColor.rgb = vec3(texCoord.x, texCoord.y, 0.0);
 	outFragColor.a = 1.0;
 }
 #endif
