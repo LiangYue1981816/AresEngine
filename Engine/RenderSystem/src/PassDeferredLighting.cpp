bdaa7a67d82a3d5db8db38e75db293084b225542
LiangYue <LiangYue1981816@Hotmail.com>
2020-04-06 18:00:23 +0800

    - 调整代码

--- Engine/RenderSystem/src/PassDeferredLighting.cpp
+++ Engine/RenderSystem/src/PassDeferredLighting.cpp
@@ -144,10 +144,9 @@ void CPassDeferredLighting::Render(CTaskPool& taskPool, CTaskGraph& taskGraph, C
 		GfxRenderer()->CmdSetImageLayout(ptrCommandBuffer, m_ptrOutputDepthStencilTexture, GFX_IMAGE_LAYOUT_GENERAL);
 		GfxRenderer()->CmdBeginRenderPass(ptrCommandBuffer, m_ptrFrameBuffer, ptrRenderPass);
 		{
-			/*
-			m_pCamera->GetRenderQueue()->CmdDraw(taskPool, taskGraph, ptrCommandBuffer, m_ptrDescriptorSetPass, PASS_FORWARD_LIGHTING_NAME, m_pCamera->GetCamera()->GetScissor(), m_pCamera->GetCamera()->GetViewport(), 0xffffffff, false);
-			m_pCamera->GetRenderQueue()->CmdDraw(taskPool, taskGraph, ptrCommandBuffer, m_ptrDescriptorSetPass, PASS_FORWARD_LIGHTING_NAME, m_pCamera->GetCamera()->GetScissor(), m_pCamera->GetCamera()->GetViewport(), 0xffffffff, true);
-			*/
+			m_pCamera->GetRenderQueue()->CmdDraw(taskPool, taskGraph, ptrCommandBuffer, m_ptrDescriptorSetPass0, PASS_DEFERRED_LIGHTING_GBUFFER_NAME, m_pCamera->GetCamera()->GetScissor(), m_pCamera->GetCamera()->GetViewport(), 0xffffffff, false);
+			ptrCommandBuffer->CmdNextSubpass();
+			m_pCamera->GetRenderQueue()->CmdDraw(taskPool, taskGraph, ptrCommandBuffer, m_ptrDescriptorSetPass0, PASS_DEFERRED_LIGHTING_SHADING_NAME, m_pCamera->GetCamera()->GetScissor(), m_pCamera->GetCamera()->GetViewport(), 0xffffffff, false);
 		}
 		GfxRenderer()->CmdEndRenderPass(ptrCommandBuffer);
 		GfxRenderer()->CmdSetImageLayout(ptrCommandBuffer, m_ptrOutputColorTexture, GFX_IMAGE_LAYOUT_COLOR_READ_ONLY_OPTIMAL);
--- Engine/RenderSystem/src/RenderDeferredLighting.cpp
+++ Engine/RenderSystem/src/RenderDeferredLighting.cpp
@@ -33,38 +33,6 @@ void CRenderSystem::RenderDeferredLighting(CTaskPool& taskPool, CTaskGraph& task
 
 void CRenderSystem::RenderDeferredLighting(CTaskPool& taskPool, CTaskGraph& taskGraph, CCamera* pCamera, bool bPresent, CGfxCommandBufferPtr ptrCommandBuffer)
 {
-
-}
-
-void CRenderSystem::RenderTileBaseDeferredLighting(CTaskPool& taskPool, CTaskGraph& taskGraph, CCamera* pCamera, bool bPresent)
-{
-	m_pInstanceBufferPool->Clear();
-
-	const CGfxSemaphore* pWaitSemaphore = GfxRenderer()->GetSwapChain()->GetAcquireSemaphore();
-	const CGfxCommandBufferPtr ptrComputeCommandBuffer = m_ptrComputeCommandBuffer[GfxRenderer()->GetSwapChain()->GetFrameIndex()];
-	const CGfxCommandBufferPtr ptrGraphicCommandBuffer = m_ptrGraphicCommandBuffer[GfxRenderer()->GetSwapChain()->GetFrameIndex()];
-
-	GfxRenderer()->AcquireNextFrame();
-	{
-		GfxRenderer()->BeginRecord(ptrComputeCommandBuffer);
-		{
-			UpdateGPUScene(taskPool, taskGraph, ptrComputeCommandBuffer);
-		}
-		GfxRenderer()->EndRecord(ptrComputeCommandBuffer);
-		GfxRenderer()->Submit(ptrComputeCommandBuffer, pWaitSemaphore);
-
-		GfxRenderer()->BeginRecord(ptrGraphicCommandBuffer);
-		{
-			RenderDeferredLighting(taskPool, taskGraph, pCamera, bPresent, ptrGraphicCommandBuffer);
-		}
-		GfxRenderer()->EndRecord(ptrGraphicCommandBuffer);
-		GfxRenderer()->Submit(ptrGraphicCommandBuffer, ptrComputeCommandBuffer->GetSemaphore());
-	}
-	GfxRenderer()->Present(ptrGraphicCommandBuffer->GetSemaphore());
-}
-
-void CRenderSystem::RenderTileBaseDeferredLighting(CTaskPool& taskPool, CTaskGraph& taskGraph, CCamera* pCamera, bool bPresent, CGfxCommandBufferPtr ptrCommandBuffer)
-{
 	uint32_t rtFinal;
 
 	uint32_t rtDepth = RENDER_TEXTURE_FULL_DEPTH;
@@ -171,4 +139,36 @@ void CRenderSystem::RenderTileBaseDeferredLighting(CTaskPool& taskPool, CTaskGra
 	m_pPassFinal->SetInputTexture(GetRenderTexture(rtFinal));
 	m_pPassFinal->SetOutputTexture(GfxRenderer()->GetSwapChain()->GetFrameIndex(), GetRenderTexture(GfxRenderer()->GetSwapChain()->GetFrameIndex()));
 	m_pPassFinal->Render(taskPool, taskGraph, ptrCommandBuffer, GfxRenderer()->GetSwapChain()->GetFrameIndex(), bPresent);
+}
+
+void CRenderSystem::RenderTileBaseDeferredLighting(CTaskPool& taskPool, CTaskGraph& taskGraph, CCamera* pCamera, bool bPresent)
+{
+	m_pInstanceBufferPool->Clear();
+
+	const CGfxSemaphore* pWaitSemaphore = GfxRenderer()->GetSwapChain()->GetAcquireSemaphore();
+	const CGfxCommandBufferPtr ptrComputeCommandBuffer = m_ptrComputeCommandBuffer[GfxRenderer()->GetSwapChain()->GetFrameIndex()];
+	const CGfxCommandBufferPtr ptrGraphicCommandBuffer = m_ptrGraphicCommandBuffer[GfxRenderer()->GetSwapChain()->GetFrameIndex()];
+
+	GfxRenderer()->AcquireNextFrame();
+	{
+		GfxRenderer()->BeginRecord(ptrComputeCommandBuffer);
+		{
+			UpdateGPUScene(taskPool, taskGraph, ptrComputeCommandBuffer);
+		}
+		GfxRenderer()->EndRecord(ptrComputeCommandBuffer);
+		GfxRenderer()->Submit(ptrComputeCommandBuffer, pWaitSemaphore);
+
+		GfxRenderer()->BeginRecord(ptrGraphicCommandBuffer);
+		{
+			RenderDeferredLighting(taskPool, taskGraph, pCamera, bPresent, ptrGraphicCommandBuffer);
+		}
+		GfxRenderer()->EndRecord(ptrGraphicCommandBuffer);
+		GfxRenderer()->Submit(ptrGraphicCommandBuffer, ptrComputeCommandBuffer->GetSemaphore());
+	}
+	GfxRenderer()->Present(ptrGraphicCommandBuffer->GetSemaphore());
+}
+
+void CRenderSystem::RenderTileBaseDeferredLighting(CTaskPool& taskPool, CTaskGraph& taskGraph, CCamera* pCamera, bool bPresent, CGfxCommandBufferPtr ptrCommandBuffer)
+{
+
 }
