79c7e85a1107dd36cfd9a8b82b8dde16c3f340a5
LiangYue <LiangYue1981816@Hotmail.com>
2020-04-07 19:32:54 +0800

    + 添加PassCopyColor.material, PassCopyDepthStencil.material, CopyColor.glsl, CopyDepthStencil.glsl

---
+++ Engine/Shader/Shader/CopyDepthStencil.glsl
@@ -0,0 +1,53 @@
+#version 310 es
+
+#ifdef VERTEX_SHADER
+precision mediump float;
+#include "engine.inc"
+#include "common.inc"
+
+USE_CAMERA_UNIFORM;
+USE_ENGINE_UNIFORM;
+
+// Output
+layout (location = 0) out mediump vec2 outTexcoord;
+
+// Descriptor
+// ...
+
+void main()
+{
+#ifdef _VULKAN_
+	highp mat4 projectionViewMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
+	highp mat4 worldMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+#else
+	highp mat4 projectionViewMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
+	highp mat4 worldMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+#endif
+	gl_Position = projectionViewMatrix * worldMatrix * vec4(inPosition.xyz, 1.0);
+	outTexcoord = inTexcoord0;
+}
+#endif
+
+#ifdef FRAGMENT_SHADER
+precision mediump float;
+#include "engine.inc"
+#include "common.inc"
+
+USE_CAMERA_UNIFORM;
+USE_ENGINE_UNIFORM;
+USE_DEPTH_TEXTURE_UNIFORM;
+
+// Input
+layout (location = 0) in mediump vec2 inTexcoord;
+
+// Output
+layout (location = 0) out mediump vec4 outFragColor;
+
+// Descriptor
+// ...
+
+void main()
+{
+	outFragColor = texture(texDepth, inTexcoord);
+}
+#endif
