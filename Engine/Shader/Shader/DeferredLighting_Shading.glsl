a97258deb3b69c0c961d0dc6d5fd4de61dec6f46
LiangYue <LiangYue1981816@Hotmail.com>
2020-04-08 23:50:21 +0800

    - 调整代码

--- Engine/Shader/Shader/DeferredLighting_Shading.glsl
+++ Engine/Shader/Shader/DeferredLighting_Shading.glsl
@@ -11,12 +11,15 @@ USE_SCENE_DATA_STORAGE;
 
 // Output
 layout (location = 0) out highp vec4 outPosition;
+layout (location = 1) flat out highp int outInstanceIndex;
 
 // Descriptor
 // ...
 
 void main()
 {
+	outInstanceIndex = inInstanceIndex;
+
 	highp mat4 worldMatrix = sceneData.data[inInstanceIndex].transformMatrix;
 	highp vec3 worldPosition = (worldMatrix * vec4(inPosition.xyz, 1.0)).xyz;
 	gl_Position = outPosition = cameraProjectionViewMatrix * vec4(worldPosition, 1.0);
@@ -30,12 +33,14 @@ precision mediump float;
 
 USE_CAMERA_UNIFORM;
 USE_ENGINE_UNIFORM;
+USE_SCENE_DATA_STORAGE;
 USE_DEPTH_TEXTURE_UNIFORM;
 
 #include "light.inc"
 
 // Input
 layout (location = 0) in highp vec4 inPosition;
+layout (location = 1) flat in highp int inInstanceIndex;
 
 // Output
 layout (location = 0) out mediump vec4 outFragColor;
