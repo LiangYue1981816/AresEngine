688aa1ba14b6d887230cfae0aa5657ecb113726f
LiangYue <LiangYue1981816@Hotmail.com>
2020-05-10 21:05:32 +0800

    - 调整代码

--- Engine/Shader/Shader/TransferSceneData.glsl
+++ Engine/Shader/Shader/TransferSceneData.glsl
@@ -12,44 +12,10 @@ USE_TRANSFER_SCENE_DATA_STORAGE;
 // ...
 
 // Descriptor
-layout(push_constant) uniform PushConstantParam {
-	int numTransfer;
-} Param;
+// ...
 
 void main()
 {
-	/*
-	uint indexWork = uint(gl_GlobalInvocationID.x);
-
-	uint numWorkGroup = uint(gl_NumWorkGroups.x);
-	uint numWorkGroupSize = uint(gl_WorkGroupSize.x);
-	uint numWorkTotalSize = uint(numWorkGroup * numWorkGroupSize);
-
-	uint numTransferTotal = uint(Param.numTransfer);
-	uint numTransferAlign = ((numTransferTotal + numWorkGroupSize - uint(1)) / numWorkGroupSize) * numWorkGroupSize;
-
-	if (numWorkTotalSize < numTransferTotal) {
-		uint numTransferWork = numTransferAlign / numWorkTotalSize;
-
-		uint indexBegin = indexWork * numTransferWork;
-		if (indexBegin >= numTransferTotal) {
-			return;
-		}
-
-		uint indexEnd = indexBegin + numTransferWork;
-		if (indexEnd > numTransferTotal) {
-			indexEnd = numTransferTotal;
-		}
-
-		for (uint index = indexBegin; index < indexEnd; index++) {
-			sceneData.data[transferSceneData.data[index].index.x] = transferSceneData.data[index].data;
-		}
-	}
-	else if (indexWork < numTransferTotal) {
-		sceneData.data[transferSceneData.data[indexWork].index.x] = transferSceneData.data[indexWork].data;
-	}
-	*/
-
 	uint indexWork = uint(gl_GlobalInvocationID.x);
 	sceneData.data[transferSceneData.data[indexWork].index.x] = transferSceneData.data[indexWork].data;
 	barrier();
