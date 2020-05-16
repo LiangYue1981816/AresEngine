141abeb362401f992867211c937244d1354e03cf
LiangYue <LiangYue1981816@Hotmail.com>
2020-05-16 17:17:28 +0800

    + 添加文件

---
+++ Engine/UtilsHeader.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "PreHeader.h"
+
+#include "Memory/include/Allocator.h"
+#include "Base/include/Task.h"
+#include "Base/include/TaskPool.h"
+#include "Base/include/TaskGraph.h"
+#include "Base/include/Lock.h"
+#include "Base/include/Utils.h"
+#include "Base/include/Stream.h"
+#include "Base/include/FileStream.h"
+#include "Base/include/MemoryStream.h"
+#include "Base/include/FileManager.h"
+#include "Base/include/SharedPtr.h"
+#include "Base/include/Delegate.h"
