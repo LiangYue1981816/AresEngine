4b2789676c06041f352c3bd5b688f92c1e86c860
LiangYue <LiangYue1981816@Hotmail.com>
2020-05-21 10:54:49 +0800

    - 调整代码

--- Engine/RenderSystem/include/Camera.h
+++ Engine/RenderSystem/include/Camera.h
@@ -42,11 +42,45 @@ private:
 
 public:
 	void SetDepthRange(float minz, float maxz);
+
 	void SetScissor(float x, float y, float width, float height);
 	void SetViewport(float x, float y, float width, float height);
+
 	void SetPerspective(float fovy, float aspect, float zNear, float zFar);
 	void SetOrtho(float left, float right, float bottom, float top, float zNear, float zFar);
 	void SetLookat(float eyex, float eyey, float eyez, float centerx, float centery, float centerz, float upx, float upy, float upz);
+
+public:
+	const float GetZNear(void) const;
+	const float GetZFar(void) const;
+
+	const float GetScissorX(void) const;
+	const float GetScissorY(void) const;
+	const float GetScissorWidth(void) const;
+	const float GetScissorHeight(void) const;
+
+	const float GetViewportX(void) const;
+	const float GetViewportY(void) const;
+	const float GetViewportWidth(void) const;
+	const float GetViewportHeight(void) const;
+
+	const glm::vec3& GetPosition(void) const;
+	const glm::vec3& GetForwardDirection(void) const;
+	const glm::vec3& GetUpDirection(void) const;
+
+	const glm::mat4& GetProjectionMatrix(void) const;
+	const glm::mat4& GetViewMatrix(void) const;
+	const glm::mat4& GetViewInverseMatrix(void) const;
+	const glm::mat4& GetViewInverseTransposeMatrix(void) const;
+
+public:
+	glm::vec3 WorldToScreen(const glm::vec3& world) const;
+	glm::vec3 ScreenToWorld(const glm::vec3& screen) const;
+
+public:
+	bool IsVisible(const glm::vec3& vertex) const;
+	bool IsVisible(const glm::aabb& aabb) const;
+	bool IsVisible(const glm::sphere& sphere) const;
 
 private:
 	void Update(CTaskPool& taskPool, CTaskGraph& taskGraph, uint32_t mask, bool bComputeLOD) const;
--- Engine/RenderSystem/src/Camera.cpp
+++ Engine/RenderSystem/src/Camera.cpp
@@ -68,6 +68,116 @@ void CCamera::SetLookat(float eyex, float eyey, float eyez, float centerx, float
 	m_pCameraUniform->SetLookat(eyex, eyey, eyez, centerx, centery, centerz, upx, upy, upz);
 }
 
+const float CCamera::GetZNear(void) const
+{
+	return m_pCamera->GetZNear();
+}
+
+const float CCamera::GetZFar(void) const
+{
+	return m_pCamera->GetZFar();
+}
+
+const float CCamera::GetScissorX(void) const
+{
+	return m_pCamera->GetScissor().x;
+}
+
+const float CCamera::GetScissorY(void) const
+{
+	return m_pCamera->GetScissor().y;
+}
+
+const float CCamera::GetScissorWidth(void) const
+{
+	return m_pCamera->GetScissor().z;
+}
+
+const float CCamera::GetScissorHeight(void) const
+{
+	return m_pCamera->GetScissor().w;
+}
+
+const float CCamera::GetViewportX(void) const
+{
+	return m_pCamera->GetViewport().x;
+}
+
+const float CCamera::GetViewportY(void) const
+{
+	return m_pCamera->GetViewport().y;
+}
+
+const float CCamera::GetViewportWidth(void) const
+{
+	return m_pCamera->GetViewport().z;
+}
+
+const float CCamera::GetViewportHeight(void) const
+{
+	return m_pCamera->GetViewport().w;
+}
+
+const glm::vec3& CCamera::GetPosition(void) const
+{
+	return m_pCamera->GetPosition();
+}
+
+const glm::vec3& CCamera::GetForwardDirection(void) const
+{
+	return m_pCamera->GetForwardDirection();
+}
+
+const glm::vec3& CCamera::GetUpDirection(void) const
+{
+	return m_pCamera->GetUpDirection();
+}
+
+const glm::mat4& CCamera::GetProjectionMatrix(void) const
+{
+	return m_pCamera->GetProjectionMatrix();
+}
+
+const glm::mat4& CCamera::GetViewMatrix(void) const
+{
+	return m_pCamera->GetViewMatrix();
+}
+
+const glm::mat4& CCamera::GetViewInverseMatrix(void) const
+{
+	return m_pCamera->GetViewInverseMatrix();
+}
+
+const glm::mat4& CCamera::GetViewInverseTransposeMatrix(void) const
+{
+	return m_pCamera->GetViewInverseTransposeMatrix();
+}
+
+glm::vec3 CCamera::WorldToScreen(const glm::vec3& world) const
+{
+	return m_pCamera->WorldToScreen(world);
+}
+
+glm::vec3 CCamera::ScreenToWorld(const glm::vec3& screen) const
+{
+	return m_pCamera->ScreenToWorld(screen);
+}
+
+bool CCamera::IsVisible(const glm::vec3& vertex) const
+{
+	return m_pCamera->IsVisible(vertex);
+}
+
+bool CCamera::IsVisible(const glm::aabb& aabb) const
+{
+	return m_pCamera->IsVisible(aabb);
+}
+
+bool CCamera::IsVisible(const glm::sphere& sphere) const
+{
+	return m_pCamera->IsVisible(sphere);
+}
+
 void CCamera::Update(CTaskPool& taskPool, CTaskGraph& taskGraph, uint32_t mask, bool bComputeLOD) const
 {
 	SceneManager()->UpdateCamera(taskPool, taskGraph, m_pCamera, m_pRenderQueue, mask, bComputeLOD);
