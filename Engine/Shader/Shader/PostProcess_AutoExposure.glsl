83b4fcd15fbc046c1638233ec2de218e8031c971
LiangYue <LiangYue1981816@Hotmail.com>
2020-07-04 15:04:47 +0800

    + 添加文件

---
+++ Engine/Shader/Shader/PostProcess_AutoExposure.glsl
@@ -0,0 +1,60 @@
+#version 310 es
+
+#ifdef VERTEX_SHADER
+precision highp float;
+#include "engine.inc"
+#include "common.inc"
+
+USE_CAMERA_UNIFORM
+USE_ENGINE_UNIFORM
+
+// Output
+layout (location = 0) out vec2 outTexcoord;
+
+// Descriptor
+// ...
+
+void main()
+{
+#ifdef _VULKAN_
+	mat4 projectionViewMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
+	mat4 worldMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+#else
+	mat4 projectionViewMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
+	mat4 worldMatrix = mat4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+#endif
+	gl_Position = projectionViewMatrix * worldMatrix * vec4(inPosition.xyz, 1.0);
+	outTexcoord = inTexcoord0;
+}
+#endif
+
+#ifdef FRAGMENT_SHADER
+precision highp float;
+#include "engine.inc"
+#include "common.inc"
+
+USE_CAMERA_UNIFORM
+USE_ENGINE_UNIFORM
+USE_COLOR_TEXTURE_UNIFORM
+
+#include "lighting.inc"
+
+// Input
+layout (location = 0) in vec2 inTexcoord;
+
+// Output
+layout (location = 0) out vec4 outFragColor;
+
+// Descriptor
+// ...
+
+void main()
+{
+	vec3 color = UnpackHDR(texture(texColor, inTexcoord));
+
+	color = ToneMapping(color);
+
+	outFragColor.rgb = color;
+	outFragColor.a = 1.0;
+}
+#endif
