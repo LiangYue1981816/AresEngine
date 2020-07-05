782040016daa471fdb4f40190793d3d6e14ccf54
LiangYue <LiangYue1981816@Hotmail.com>
2020-07-05 18:45:12 +0800

    - 调整代码

--- Engine/RenderSystem/include/GPUEyeHistogram.h
+++ Engine/RenderSystem/include/GPUEyeHistogram.h
@@ -1,45 +1,30 @@
 #pragma once
 #include "PreHeader.h"
 
-/*
-class CALL_API CPassAutoExposure : public CPassBlit
+
+class CALL_API CGPUEyeHistogram
 {
-	friend class CCamera;
 	friend class CRenderSystem;
 
 
 private:
-	static void Create(GfxPixelFormat colorPixelFormat);
-	static void Destroy(void);
-
-
-private:
-	CPassAutoExposure(CRenderSystem* pRenderSystem);
-	virtual ~CPassAutoExposure(void);
+	CGPUEyeHistogram(CRenderSystem* pRenderSystem);
+	virtual ~CGPUEyeHistogram(void);
 
 
 public:
-	void SetCamera(CCamera* pCamera);
-	void SetInputReferenceTexture(CGfxRenderTexturePtr ptrInputReferenceTexture);
 	void SetInputTexture(CGfxRenderTexturePtr ptrColorTexture);
-	void SetOutputTexture(CGfxRenderTexturePtr ptrColorTexture);
 
 private:
-	void Render(CTaskPool& taskPool, CTaskGraph& taskGraph, CGfxCommandBufferPtr ptrCommandBuffer);
+	void Compute(CTaskPool& taskPool, CTaskGraph& taskGraph, CGfxCommandBufferPtr ptrCommandBuffer);
 
 
 private:
 	CGfxShader* m_pShaderCompute;
 	CGfxPipelineCompute* m_pPipelineCompute;
 	CGfxDescriptorSetPtr m_ptrDescriptorSet;
-	CGfxRenderTexturePtr m_ptrInputReferenceTexture;
-
-private:
-	CGfxDescriptorSetPtr m_ptrDescriptorSetPass;
 	CGfxRenderTexturePtr m_ptrInputColorTexture;
 
 private:
-	CGfxFrameBufferPtr m_ptrFrameBuffer;
-	CGfxRenderTexturePtr m_ptrOutputColorTexture;
+	CRenderSystem* m_pRenderSystem;
 };
-*/
