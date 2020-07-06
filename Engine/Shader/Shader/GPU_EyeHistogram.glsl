a83abf0e88cee3ae9e9e819bba1c227b456d2fe7
LiangYue <LiangYue1981816@Hotmail.com>
2020-07-06 21:28:33 +0800

    - 调整代码

--- Engine/Shader/Shader/GPU_EyeHistogram.glsl
+++ Engine/Shader/Shader/GPU_EyeHistogram.glsl
@@ -1,10 +1,11 @@
 #version 310 es
-layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
 precision highp float;
 #include "engine.inc"
 #include "common.inc"
 
+USE_HISTOGRAM_STORAGE
 USE_COLOR_TEXTURE_UNIFORM
 
 // Output
@@ -13,7 +14,40 @@ USE_COLOR_TEXTURE_UNIFORM
 // Descriptor
 // ...
 
+// Shared
+shared uint share_histogram[64];
+
+
+float GetHistogram(float linear, float minLogValue, float maxLogValue)
+{
+	return clamp((log2(linear) - minLogValue) / (maxLogValue - minLogValue), 0.0, 1.0);
+}
+
 void main()
 {
+	const float minLinearValue = 0.001;
+	const float maxLinearValue = 4.0;
 
+	const float minLogValue = log2(minLinearValue);
+	const float maxLogValue = log2(maxLinearValue);
+
+	if (gl_LocalInvocationIndex < uint(64)) {
+		share_histogram[gl_LocalInvocationIndex] = uint(0);
+	}
+
+	barrier();
+
+	vec2 texCoord = vec2(gl_GlobalInvocationID.xy) / vec2(textureSize(texColor, 0));
+	vec3 color = texture(texColor, texCoord).rgb;
+
+	float l = clamp(max(color.r, max(color.g, color.b)), minLinearValue, maxLinearValue);
+	float h = GetHistogram(l, minLogValue, maxLogValue);
+
+	atomicAdd(share_histogram[int(h * (64.0 - 1.0))], uint(1));
+
+	barrier();
+
+	if (gl_LocalInvocationIndex < uint(64)) {
+		atomicAdd(histogramData.histogram[gl_LocalInvocationIndex], share_histogram[gl_LocalInvocationIndex]);
+	}
 }
