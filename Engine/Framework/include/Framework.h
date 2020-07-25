b3e5d3fe35cb69fe75fe4bb1e905ff59fe3e79e6
LiangYue <LiangYue1981816@Hotmail.com>
2020-07-25 14:36:22 +0800

    + 添加文件

--- Engine/Framework/include/Framework.h
+++ Engine/Framework/include/Framework.h
@@ -2,3 +2,19 @@
 #include "PreHeader.h"
 
 
+class CFramework
+{
+public:
+	static CFramework* GetInstance(void);
+	static void Create(void);
+	static void Destroy(void);
+
+
+private:
+	CFramework(void);
+	virtual ~CFramework(void);
+
+
+private:
+	static CFramework* pInstance;
+};
---
+++ Engine/Framework/src/Framework.cpp
@@ -0,0 +1,45 @@
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+
+#include "Application.h"
+#include "EngineHeader.h"
+
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
+CApplication::CApplication(void)
+	: m_hDC(nullptr)
+	, m_width(0)
+	, m_height(0)
+{
+
+}
+
+CApplication::~CApplication(void)
+{
+
+}
+
+void CApplication::Update(void)
+{
+	static uint32_t lastTick = 0;
+	float deltaTime = (Tick() - lastTick) / 1000000.0f;
+
+	if (deltaTime > 1.0f / 60.0f) {
+		static bool bFirstFrame = true;
+		{
+			if (bFirstFrame == false) {
+				UpdateInternal(deltaTime);
+			}
+		}
+		bFirstFrame = false;
+
+		lastTick = Tick();
+	}
+}
+
+int64_t CApplication::WndProc(void* hWnd, uint32_t msg, uint64_t wParam, int64_t lParam)
+{
+	return ImGui_ImplWin32_WndProcHandler((HWND)hWnd, msg, wParam, lParam);
+}
