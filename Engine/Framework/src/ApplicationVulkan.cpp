762a625c4155dcac0d2a34ff9a6fc8e71ba262be
LiangYue <LiangYue1981816@Hotmail.com>
2020-07-27 21:25:16 +0800

    - 调整代码

--- Engine/Framework/src/ApplicationVulkan.cpp
+++ Engine/Framework/src/ApplicationVulkan.cpp
@@ -149,68 +149,49 @@ static void ImGui_ImplVulkan_DestroyWindow(VkAllocationCallbacks* allocator, VkR
 	*render_pass = VK_NULL_HANDLE;
 }
 
-static void ImGui_ImplVulkan_FrameRender(ImGui_ImplVulkanH_Window* wd, ImDrawData* draw_data)
+static void ImGui_ImplVulkan_FrameRender(VkCommandBuffer command_buffer, VkFence fence, VkSemaphore wait_semaphore, VkSemaphore signal_semaphore, VkRenderPass render_pass, VkFramebuffer frame_buffer, int width, int height, ImDrawData* draw_data)
 {
-	/*
-	VkResult err;
-
-	VkSemaphore image_acquired_semaphore = wd->FrameSemaphores[wd->SemaphoreIndex].ImageAcquiredSemaphore;
-	VkSemaphore render_complete_semaphore = wd->FrameSemaphores[wd->SemaphoreIndex].RenderCompleteSemaphore;
-	err = vkAcquireNextImageKHR(g_Device, wd->Swapchain, UINT64_MAX, image_acquired_semaphore, VK_NULL_HANDLE, &wd->FrameIndex);
-	check_vk_result(err);
-
-	ImGui_ImplVulkanH_Frame* fd = &wd->Frames[wd->FrameIndex];
 	{
-		err = vkWaitForFences(g_Device, 1, &fd->Fence, VK_TRUE, UINT64_MAX);    // wait indefinitely instead of periodically checking
-		check_vk_result(err);
-
-		err = vkResetFences(g_Device, 1, &fd->Fence);
-		check_vk_result(err);
+		check_vk_result(vkWaitForFences(VKRenderer()->GetDevice(), 1, &fence, VK_TRUE, UINT64_MAX));
+		check_vk_result(vkResetFences(VKRenderer()->GetDevice(), 1, &fence));
 	}
 	{
-		err = vkResetCommandPool(g_Device, fd->CommandPool, 0);
-		check_vk_result(err);
+		check_vk_result(vkResetCommandBuffer(command_buffer, VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT));
+
 		VkCommandBufferBeginInfo info = {};
 		info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 		info.flags |= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-		err = vkBeginCommandBuffer(fd->CommandBuffer, &info);
-		check_vk_result(err);
+		check_vk_result(vkBeginCommandBuffer(command_buffer, &info));
 	}
 	{
+		VkClearValue clear_value;
 		VkRenderPassBeginInfo info = {};
 		info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-		info.renderPass = wd->RenderPass;
-		info.framebuffer = fd->Framebuffer;
-		info.renderArea.extent.width = wd->Width;
-		info.renderArea.extent.height = wd->Height;
+		info.renderPass = render_pass;
+		info.framebuffer = frame_buffer;
+		info.renderArea.extent.width = width;
+		info.renderArea.extent.height = height;
 		info.clearValueCount = 1;
-		info.pClearValues = &wd->ClearValue;
-		vkCmdBeginRenderPass(fd->CommandBuffer, &info, VK_SUBPASS_CONTENTS_INLINE);
+		info.pClearValues = &clear_value;
+		vkCmdBeginRenderPass(command_buffer, &info, VK_SUBPASS_CONTENTS_INLINE);
 	}
-
-	// Record dear imgui primitives into command buffer
-	ImGui_ImplVulkan_RenderDrawData(draw_data, fd->CommandBuffer);
-
-	// Submit command buffer
-	vkCmdEndRenderPass(fd->CommandBuffer);
+	ImGui_ImplVulkan_RenderDrawData(draw_data, command_buffer);
 	{
+		vkCmdEndRenderPass(command_buffer);
+
 		VkPipelineStageFlags wait_stage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
 		VkSubmitInfo info = {};
 		info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
 		info.waitSemaphoreCount = 1;
-		info.pWaitSemaphores = &image_acquired_semaphore;
+		info.pWaitSemaphores = &wait_semaphore;
 		info.pWaitDstStageMask = &wait_stage;
 		info.commandBufferCount = 1;
-		info.pCommandBuffers = &fd->CommandBuffer;
+		info.pCommandBuffers = &command_buffer;
 		info.signalSemaphoreCount = 1;
-		info.pSignalSemaphores = &render_complete_semaphore;
-
-		err = vkEndCommandBuffer(fd->CommandBuffer);
-		check_vk_result(err);
-		err = vkQueueSubmit(g_Queue, 1, &info, fd->Fence);
-		check_vk_result(err);
+		info.pSignalSemaphores = &signal_semaphore;
+		check_vk_result(vkEndCommandBuffer(command_buffer));
+		check_vk_result(vkQueueSubmit(VKRenderer()->GetQueue(), 1, &info, fence));
 	}
-	*/
 }
 
 
