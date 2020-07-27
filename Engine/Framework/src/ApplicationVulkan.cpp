6246df797729b9aba2b9af8ff903e256b8290afe
LiangYue <LiangYue1981816@Hotmail.com>
2020-07-27 20:16:06 +0800

    - 调整代码

--- Engine/Framework/src/ApplicationVulkan.cpp
+++ Engine/Framework/src/ApplicationVulkan.cpp
@@ -7,9 +7,8 @@
 #include "VKRenderer.h"
 
 
-void UpdateInput(void);
-void UpdateRenderSolution(void);
-
+extern void UpdateInput(void);
+extern void UpdateRenderSolution(void);
 
 static VkDescriptorPool vkDescriptorPool = VK_NULL_HANDLE;
 static VkRenderPass vkRenderPass = VK_NULL_HANDLE;
@@ -253,67 +252,69 @@ void CApplicationVulkan::Destroy(void)
 
 void CApplicationVulkan::UpdateInternal(float deltaTime)
 {
+	const CGfxSemaphore* pWaitSemaphore = GfxRenderer()->GetSwapChain()->GetAcquireSemaphore();
+	const CGfxCommandBufferPtr ptrComputeCommandBuffer = ptrComputeCommandBuffers[GfxRenderer()->GetSwapChain()->GetFrameIndex()];
+	const CGfxCommandBufferPtr ptrGraphicCommandBuffer = ptrGraphicCommandBuffers[GfxRenderer()->GetSwapChain()->GetFrameIndex()];
+	const CGfxCommandBufferPtr ptrImGuiCommandBuffer = ptrImGuiCommandBuffers[GfxRenderer()->GetSwapChain()->GetFrameIndex()];
 
-}
-
-void UpdateInput(void)
-{
-	// Mouse
+	GfxRenderer()->AcquireNextFrame();
 	{
-		ImVec2 curMousePos = ImGui::GetMousePos();
-		int x = (int)curMousePos.x;
-		int y = (int)curMousePos.y;
+		ImGui_ImplVulkan_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
 
-		if (ImGui::IsMouseDown(ImGuiMouseButton_Left) && ImGui::IsAnyWindowHovered() == false) {
-			Framework()->OnLButtonDown(x, y);
+		static bool show_demo_window = true;
+		static bool show_another_window = false;
+		static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+		// Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+
+			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
 		}
 
-		if (ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
-			Framework()->OnLButtonRelease(x, y);
+		// Show another simple window.
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
+			ImGui::End();
 		}
 
-		Framework()->OnMouseMove(x, y);
+		// Update
+		Engine()->Wait();
+		{
+			UpdateInput();
+			UpdateRenderSolution();
+			Framework()->Update(deltaTime);
+		}
+		Engine()->Update();
+
+		// Render Scene
+		Framework()->Render(ptrComputeCommandBuffer, ptrGraphicCommandBuffer, pWaitSemaphore);
+
+		// Render ImGui
+//		ImGui::Render();
+//		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	}
-
-	// Key
-	{
-		if (ImGui::IsKeyDown('W')) Framework()->OnKeyDown('W');
-		if (ImGui::IsKeyDown('S')) Framework()->OnKeyDown('S');
-		if (ImGui::IsKeyDown('A')) Framework()->OnKeyDown('A');
-		if (ImGui::IsKeyDown('D')) Framework()->OnKeyDown('D');
-		if (ImGui::IsKeyDown('E')) Framework()->OnKeyDown('E');
-		if (ImGui::IsKeyDown('Q')) Framework()->OnKeyDown('Q');
-
-		if (ImGui::IsKeyReleased('W')) Framework()->OnKeyRelease('W');
-		if (ImGui::IsKeyReleased('S')) Framework()->OnKeyRelease('S');
-		if (ImGui::IsKeyReleased('A')) Framework()->OnKeyRelease('A');
-		if (ImGui::IsKeyReleased('D')) Framework()->OnKeyRelease('D');
-		if (ImGui::IsKeyReleased('E')) Framework()->OnKeyRelease('E');
-		if (ImGui::IsKeyReleased('Q')) Framework()->OnKeyRelease('Q');
-	}
-}
-
-void UpdateRenderSolution(void)
-{
-	float shRed[9] = { 0.588794f, 0.254506f, 0.082485f, 0.028278f, 0.036788f, 0.086514f, 0.018185f, 0.021567f, 0.006525f };
-	float shGreen[9] = { 0.599826f, 0.275154f, 0.076468f, 0.028166f, 0.032798f, 0.082039f, 0.014064f, 0.020624f, 0.001984f };
-	float shBlue[9] = { 0.558644f, 0.387983f, 0.073534f, 0.024760f, 0.029393f, 0.076786f, 0.008863f, 0.017515f, -0.004231f };
-
-	static float angle = 0.0f; angle += 0.002f;
-	glm::vec4 directLightDirection = glm::rotate(glm::mat4(), angle, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(0.35f, -1.0f, 0.0f, 0.0f);
-
-	RenderSystem()->SetEnvLightFactor(1.5f);
-
-	RenderSystem()->SetAmbientLightFactor(0.5f);
-	RenderSystem()->SetAmbientLightSH(shRed, shGreen, shBlue);
-
-	RenderSystem()->SetMainDirectLightFactor(1.5f);
-	RenderSystem()->SetMainDirectLightColor(3.5f, 3.5f, 3.5f);
-	RenderSystem()->SetMainDirectLightDirection(directLightDirection.x, directLightDirection.y, directLightDirection.z);
-
-	Settings()->SetValue("RenderSystem.Shadow.Factor", 1.0f);
-	Settings()->SetValue("RenderSystem.Shadow.SplitFactor0", exp(-4.0f));
-	Settings()->SetValue("RenderSystem.Shadow.SplitFactor1", exp(-3.0f));
-	Settings()->SetValue("RenderSystem.Shadow.SplitFactor2", exp(-2.0f));
-	Settings()->SetValue("RenderSystem.Shadow.SplitFactor3", exp(-1.0f));
+	GfxRenderer()->Present(ptrGraphicCommandBuffer->GetSemaphore());
 }
