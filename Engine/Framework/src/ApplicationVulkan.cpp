965968b165d9eec823f32d7c62bda35c26406210
LiangYue <LiangYue1981816@Hotmail.com>
2020-07-27 13:01:58 +0800

    - 调整代码

--- Engine/Framework/src/ApplicationVulkan.cpp
+++ Engine/Framework/src/ApplicationVulkan.cpp
@@ -11,6 +11,94 @@ static CGfxCommandBufferPtr ptrComputeCommandBuffers[CGfxSwapChain::SWAPCHAIN_FR
 static CGfxCommandBufferPtr ptrGraphicCommandBuffers[CGfxSwapChain::SWAPCHAIN_FRAME_COUNT];
 
 
+static void check_vk_result(VkResult err)
+{
+	if (err == 0)
+		return;
+
+	printf("[vulkan] Error: VkResult = %d\n", err);
+
+	if (err < 0)
+		abort();
+}
+
+static void ImGui_ImplVulkan_CreateWindow(VkDevice device, uint32_t width, uint32_t height, VkFormat format, VkAllocationCallbacks* allocator, VkImage images[3], VkRenderPass* pRenderPass, VkImageView* pImageViews[3], VkFramebuffer* pFramebuffers[3])
+{
+	{
+		VkAttachmentDescription attachment = {};
+		attachment.format = format;
+		attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+		attachment.loadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+		VkAttachmentReference color_attachment = {};
+		color_attachment.attachment = 0;
+		color_attachment.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+		VkSubpassDescription subpass = {};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &color_attachment;
+		VkSubpassDependency dependency = {};
+		dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+		dependency.dstSubpass = 0;
+		dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+		dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+		dependency.srcAccessMask = 0;
+		dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+		VkRenderPassCreateInfo info = {};
+		info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		info.attachmentCount = 1;
+		info.pAttachments = &attachment;
+		info.subpassCount = 1;
+		info.pSubpasses = &subpass;
+		info.dependencyCount = 1;
+		info.pDependencies = &dependency;
+		check_vk_result(vkCreateRenderPass(device, &info, allocator, pRenderPass));
+	}
+	{
+		VkImageViewCreateInfo info = {};
+		info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		info.format = format;
+		info.components.r = VK_COMPONENT_SWIZZLE_R;
+		info.components.g = VK_COMPONENT_SWIZZLE_G;
+		info.components.b = VK_COMPONENT_SWIZZLE_B;
+		info.components.a = VK_COMPONENT_SWIZZLE_A;
+		VkImageSubresourceRange image_range = { VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 };
+		info.subresourceRange = image_range;
+		for (uint32_t i = 0; i < 3; i++)
+		{
+			info.image = images[i];
+			check_vk_result(vkCreateImageView(device, &info, allocator, pImageViews[i]));
+		}
+	}
+	{
+		VkImageView attachment[1];
+		VkFramebufferCreateInfo info = {};
+		info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+		info.renderPass = *pRenderPass;
+		info.attachmentCount = 1;
+		info.pAttachments = attachment;
+		info.width = width;
+		info.height = height;
+		info.layers = 1;
+		for (uint32_t i = 0; i < 3; i++)
+		{
+			attachment[0] = *pImageViews[i];
+			check_vk_result(vkCreateFramebuffer(device, &info, allocator, pFramebuffers[i]));
+		}
+	}
+}
+
+static void ImGui_ImplVulkan_CreateCommandBuffer()
+{
+
+}
+
+
 CApplicationVulkan::CApplicationVulkan(void)
 {
 
