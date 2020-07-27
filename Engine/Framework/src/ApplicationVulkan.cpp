c706028eddf58647949929354fdcac94030c049b
LiangYue <LiangYue1981816@Hotmail.com>
2020-07-27 20:06:46 +0800

    - 调整代码

--- Engine/Framework/src/ApplicationVulkan.cpp
+++ Engine/Framework/src/ApplicationVulkan.cpp
@@ -7,6 +7,10 @@
 #include "VKRenderer.h"
 
 
+void UpdateInput(void);
+void UpdateRenderSolution(void);
+
+
 static VkDescriptorPool vkDescriptorPool = VK_NULL_HANDLE;
 static VkRenderPass vkRenderPass = VK_NULL_HANDLE;
 static VkImageView vkImageViews[3] = { VK_NULL_HANDLE , VK_NULL_HANDLE , VK_NULL_HANDLE };
@@ -250,4 +254,66 @@ void CApplicationVulkan::Destroy(void)
 void CApplicationVulkan::UpdateInternal(float deltaTime)
 {
 
+}
+
+void UpdateInput(void)
+{
+	// Mouse
+	{
+		ImVec2 curMousePos = ImGui::GetMousePos();
+		int x = (int)curMousePos.x;
+		int y = (int)curMousePos.y;
+
+		if (ImGui::IsMouseDown(ImGuiMouseButton_Left) && ImGui::IsAnyWindowHovered() == false) {
+			Framework()->OnLButtonDown(x, y);
+		}
+
+		if (ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
+			Framework()->OnLButtonRelease(x, y);
+		}
+
+		Framework()->OnMouseMove(x, y);
+	}
+
+	// Key
+	{
+		if (ImGui::IsKeyDown('W')) Framework()->OnKeyDown('W');
+		if (ImGui::IsKeyDown('S')) Framework()->OnKeyDown('S');
+		if (ImGui::IsKeyDown('A')) Framework()->OnKeyDown('A');
+		if (ImGui::IsKeyDown('D')) Framework()->OnKeyDown('D');
+		if (ImGui::IsKeyDown('E')) Framework()->OnKeyDown('E');
+		if (ImGui::IsKeyDown('Q')) Framework()->OnKeyDown('Q');
+
+		if (ImGui::IsKeyReleased('W')) Framework()->OnKeyRelease('W');
+		if (ImGui::IsKeyReleased('S')) Framework()->OnKeyRelease('S');
+		if (ImGui::IsKeyReleased('A')) Framework()->OnKeyRelease('A');
+		if (ImGui::IsKeyReleased('D')) Framework()->OnKeyRelease('D');
+		if (ImGui::IsKeyReleased('E')) Framework()->OnKeyRelease('E');
+		if (ImGui::IsKeyReleased('Q')) Framework()->OnKeyRelease('Q');
+	}
+}
+
+void UpdateRenderSolution(void)
+{
+	float shRed[9] = { 0.588794f, 0.254506f, 0.082485f, 0.028278f, 0.036788f, 0.086514f, 0.018185f, 0.021567f, 0.006525f };
+	float shGreen[9] = { 0.599826f, 0.275154f, 0.076468f, 0.028166f, 0.032798f, 0.082039f, 0.014064f, 0.020624f, 0.001984f };
+	float shBlue[9] = { 0.558644f, 0.387983f, 0.073534f, 0.024760f, 0.029393f, 0.076786f, 0.008863f, 0.017515f, -0.004231f };
+
+	static float angle = 0.0f; angle += 0.002f;
+	glm::vec4 directLightDirection = glm::rotate(glm::mat4(), angle, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(0.35f, -1.0f, 0.0f, 0.0f);
+
+	RenderSystem()->SetEnvLightFactor(1.5f);
+
+	RenderSystem()->SetAmbientLightFactor(0.5f);
+	RenderSystem()->SetAmbientLightSH(shRed, shGreen, shBlue);
+
+	RenderSystem()->SetMainDirectLightFactor(1.5f);
+	RenderSystem()->SetMainDirectLightColor(3.5f, 3.5f, 3.5f);
+	RenderSystem()->SetMainDirectLightDirection(directLightDirection.x, directLightDirection.y, directLightDirection.z);
+
+	Settings()->SetValue("RenderSystem.Shadow.Factor", 1.0f);
+	Settings()->SetValue("RenderSystem.Shadow.SplitFactor0", exp(-4.0f));
+	Settings()->SetValue("RenderSystem.Shadow.SplitFactor1", exp(-3.0f));
+	Settings()->SetValue("RenderSystem.Shadow.SplitFactor2", exp(-2.0f));
+	Settings()->SetValue("RenderSystem.Shadow.SplitFactor3", exp(-1.0f));
 }
