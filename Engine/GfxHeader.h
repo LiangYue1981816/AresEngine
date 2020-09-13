49537d6f07ed27eec01cd297809da22b974fc7d8
LiangYue <LiangYue1981816@Hotmail.com>
2020-09-13 18:19:57 +0800

    - 调整代码

--- Engine/GfxHeader.h
+++ Engine/GfxHeader.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "PreHeader.h"
 #include "BaseHeader.h"
-#include "ConsoleHeader.h"
 
 #include "Renderer/Gfx/include/GfxRenderer.h"
 #include "Renderer/Compiler/include/ShaderCompiler.h"
