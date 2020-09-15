0fce83eb95ada8da1c95bb41a87e99d08f9d3163
LiangYue <LiangYue1981816@Hotmail.com>
2020-09-15 21:07:45 +0800

    + 添加代码

--- Engine/Renderer/Vulkan/src/Command/VKCommandBufferData.h
+++ Engine/Renderer/Vulkan/src/Command/VKCommandBufferData.h
@@ -5,8 +5,39 @@
 class CVKCommandBufferData : public CGfxCommandBase
 {
 public:
-	CVKCommandBufferData(VkCommandBuffer vkCommandBuffer)
+	CVKCommandBufferData(VkCommandBuffer vkCommandBuffer, const CGfxIndexBufferPtr ptrBuffer, size_t offset, size_t size, const void* data)
 		: m_vkCommandBuffer(vkCommandBuffer)
+		, m_ptrIndexBuffer(ptrBuffer)
+	{
+		Execute();
+	}
+	CVKCommandBufferData(VkCommandBuffer vkCommandBuffer, const CGfxVertexBufferPtr ptrBuffer, size_t offset, size_t size, const void* data)
+		: m_vkCommandBuffer(vkCommandBuffer)
+		, m_ptrVertexBuffer(ptrBuffer)
+	{
+		Execute();
+	}
+	CVKCommandBufferData(VkCommandBuffer vkCommandBuffer, const CGfxInstanceBufferPtr ptrBuffer, size_t offset, size_t size, const void* data)
+		: m_vkCommandBuffer(vkCommandBuffer)
+		, m_ptrInstanceBuffer(ptrBuffer)
+	{
+		Execute();
+	}
+	CVKCommandBufferData(VkCommandBuffer vkCommandBuffer, const CGfxIndirectBufferPtr ptrBuffer, size_t offset, size_t size, const void* data)
+		: m_vkCommandBuffer(vkCommandBuffer)
+		, m_ptrIndirectBuffer(ptrBuffer)
+	{
+		Execute();
+	}
+	CVKCommandBufferData(VkCommandBuffer vkCommandBuffer, const CGfxUniformBufferPtr ptrBuffer, size_t offset, size_t size, const void* data)
+		: m_vkCommandBuffer(vkCommandBuffer)
+		, m_ptrUniformBuffer(ptrBuffer)
+	{
+		Execute();
+	}
+	CVKCommandBufferData(VkCommandBuffer vkCommandBuffer, const CGfxStorageBufferPtr ptrBuffer, size_t offset, size_t size, const void* data)
+		: m_vkCommandBuffer(vkCommandBuffer)
+		, m_ptrStorageBuffer(ptrBuffer)
 	{
 		Execute();
 	}
@@ -23,6 +54,13 @@ public:
 
 
 private:
+	CGfxIndexBufferPtr m_ptrIndexBuffer;
+	CGfxVertexBufferPtr m_ptrVertexBuffer;
+	CGfxInstanceBufferPtr m_ptrInstanceBuffer;
+	CGfxIndirectBufferPtr m_ptrIndirectBuffer;
+	CGfxUniformBufferPtr m_ptrUniformBuffer;
+	CGfxStorageBufferPtr m_ptrStorageBuffer;
+	CGfxTransferBufferPtr m_ptrTransferBuffer;
 
 private:
 	VkCommandBuffer m_vkCommandBuffer;
