8c8f7bb2fd8f2485e3a18c4cd96d762cd5f78125
LiangYue <LiangYue1981816@Hotmail.com>
2020-09-17 22:03:12 +0800

    - 调整代码

--- Engine/Renderer/Gfx/include/GfxDefinition.h
+++ Engine/Renderer/Gfx/include/GfxDefinition.h
@@ -3,23 +3,17 @@
 
 class CGfxProfiler;
 class CGfxProfilerSample;
-
 class CGfxResource;
 class CGfxResourceManager;
-
 class CGfxCamera;
-
 class CGfxRenderer;
-
 class CGfxSwapChain;
 class CGfxRenderPass;
 class CGfxRenderTexture;
 class CGfxFrameBuffer;
-
 class CGfxShader;
 class CGfxPipelineCompute;
 class CGfxPipelineGraphics;
-
 class CGfxIndexBuffer;
 class CGfxVertexBuffer;
 class CGfxUniformBuffer;
@@ -27,27 +21,21 @@ class CGfxStorageBuffer;
 class CGfxInstanceBuffer;
 class CGfxIndirectBuffer;
 class CGfxTransferBuffer;
-
 class CGfxSampler;
 class CGfxTexture2D;
 class CGfxTexture2DArray;
 class CGfxTextureCubemap;
-
 class CGfxUniformVec1;
 class CGfxUniformVec2;
 class CGfxUniformVec3;
 class CGfxUniformVec4;
 class CGfxUniformMat4;
-
 class CGfxMesh;
 class CGfxMeshDraw;
-
 class CGfxMaterial;
 class CGfxMaterialPass;
-
 class CGfxDescriptorSet;
 class CGfxDescriptorLayout;
-
 class CGfxSemaphore;
 class CGfxCommandBuffer;
 
@@ -58,7 +46,6 @@ class CGfxResourcePtr;
 typedef CGfxResourcePtr<CGfxRenderPass> CGfxRenderPassPtr;
 typedef CGfxResourcePtr<CGfxRenderTexture> CGfxRenderTexturePtr;
 typedef CGfxResourcePtr<CGfxFrameBuffer> CGfxFrameBufferPtr;
-
 typedef CGfxResourcePtr<CGfxIndexBuffer> CGfxIndexBufferPtr;
 typedef CGfxResourcePtr<CGfxVertexBuffer> CGfxVertexBufferPtr;
 typedef CGfxResourcePtr<CGfxUniformBuffer> CGfxUniformBufferPtr;
@@ -66,16 +53,12 @@ typedef CGfxResourcePtr<CGfxStorageBuffer> CGfxStorageBufferPtr;
 typedef CGfxResourcePtr<CGfxInstanceBuffer> CGfxInstanceBufferPtr;
 typedef CGfxResourcePtr<CGfxIndirectBuffer> CGfxIndirectBufferPtr;
 typedef CGfxResourcePtr<CGfxTransferBuffer> CGfxTransferBufferPtr;
-
 typedef CGfxResourcePtr<CGfxTexture2D> CGfxTexture2DPtr;
 typedef CGfxResourcePtr<CGfxTexture2DArray> CGfxTexture2DArrayPtr;
 typedef CGfxResourcePtr<CGfxTextureCubemap> CGfxTextureCubemapPtr;
-
 typedef CGfxResourcePtr<CGfxMesh> CGfxMeshPtr;
 typedef CGfxResourcePtr<CGfxMeshDraw> CGfxMeshDrawPtr;
 typedef CGfxResourcePtr<CGfxMaterial> CGfxMaterialPtr;
-
 typedef CGfxResourcePtr<CGfxDescriptorSet> CGfxDescriptorSetPtr;
 typedef CGfxResourcePtr<CGfxDescriptorLayout> CGfxDescriptorLayoutPtr;
-
 typedef CGfxResourcePtr<CGfxCommandBuffer> CGfxCommandBufferPtr;
