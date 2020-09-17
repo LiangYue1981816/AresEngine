1eaff102ff6b81f50bb2ae0417ca65cc2e8282ce
LiangYue <LiangYue1981816@Hotmail.com>
2020-09-17 11:17:27 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VKStorageBuffer.cpp
+++ Engine/Renderer/Vulkan/src/VKStorageBuffer.cpp
@@ -3,8 +3,8 @@
 
 CVKStorageBuffer::CVKStorageBuffer(CVKDevice* pDevice, CVKStorageBufferManager* pManager, size_t size)
 	: CGfxStorageBuffer(size)
-	, m_pDevice(pDevice)
 	, m_pManager(pManager)
+	, m_pDevice(pDevice)
 	, m_pBuffer(nullptr)
 {
 	size = ALIGN_BYTE(size, m_pDevice->GetPhysicalDeviceLimits().nonCoherentAtomSize);
@@ -22,7 +22,12 @@ CVKStorageBuffer::~CVKStorageBuffer(void)
 
 void CVKStorageBuffer::Release(void)
 {
-	m_pManager->Destroy(this);
+	if (m_pManager) {
+		m_pManager->Destroy(this);
+	}
+	else {
+		delete this;
+	}
 }
 
 VkBuffer CVKStorageBuffer::GetBuffer(void) const
@@ -35,23 +40,17 @@ VkBufferUsageFlags CVKStorageBuffer::GetBufferUsageFlags(void) const
 	return m_pBuffer->GetBufferUsageFlags();
 }
 
-uint32_t CVKStorageBuffer::GetSize(void) const
+VkDeviceSize CVKStorageBuffer::GetBufferSize(void) const
 {
-	return m_pBuffer->GetSize();
+	return m_pBuffer->GetBufferSize();
+}
+
+VkDeviceSize CVKStorageBuffer::GetMemorySize(void) const
+{
+	return m_pBuffer->GetMemorySize();
 }
 
 bool CVKStorageBuffer::BufferData(size_t offset, size_t size, const void* data)
 {
 	return m_pBuffer->BufferData(offset, size, data);
 }
-/*
-bool CVKStorageBuffer::PipelineBarrier(VkCommandBuffer vkCommandBuffer, VkAccessFlags srcAccessFlags, VkAccessFlags dstAccessFlags, VkDeviceSize offset, VkDeviceSize size)
-{
-	return m_pBuffer->PipelineBarrier(vkCommandBuffer, srcAccessFlags, dstAccessFlags, offset, size);
-}
-
-bool CVKStorageBuffer::PipelineBarrier(VkCommandBuffer vkCommandBuffer, VkAccessFlags srcAccessFlags, VkAccessFlags dstAccessFlags, VkPipelineStageFlags srcPipelineStageFlags, VkPipelineStageFlags dstPipelineStageFlags, VkDeviceSize offset, VkDeviceSize size)
-{
-	return m_pBuffer->PipelineBarrier(vkCommandBuffer, srcAccessFlags, dstAccessFlags, srcPipelineStageFlags, dstPipelineStageFlags, offset, size);
-}
-*/
