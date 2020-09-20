ee2439224000343e3fc1977fdaab61abfbf189f3
LiangYue <LiangYue1981816@Hotmail.com>
2020-09-20 11:51:37 +0800

    - 调整代码

--- Engine/Renderer/Vulkan/src/VKFrameBuffer.cpp
+++ Engine/Renderer/Vulkan/src/VKFrameBuffer.cpp
@@ -47,39 +47,34 @@ int CVKFrameBuffer::GetHeight(void) const
 
 bool CVKFrameBuffer::Create(const CGfxRenderPassPtr ptrRenderPass)
 {
-	Destroy(false);
-	{
-		do {
-			ASSERT(ptrRenderPass);
-
-			eastl::vector<VkImageView> attachments;
-			{
-				for (int indexAttachment = 0; indexAttachment < m_ptrAttachmentTextures.size(); indexAttachment++) {
-					attachments.emplace_back(((CVKRenderTexture*)m_ptrAttachmentTextures[indexAttachment].GetPointer())->GetImageView());
-				}
-
-				if (attachments.empty()) {
-					break;
-				}
-			}
-
-			VkFramebufferCreateInfo frameBufferCreateInfo = {};
-			frameBufferCreateInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
-			frameBufferCreateInfo.pNext = nullptr;
-			frameBufferCreateInfo.flags = 0;
-			frameBufferCreateInfo.renderPass = ((CVKRenderPass*)ptrRenderPass.GetPointer())->GetRenderPass();
-			frameBufferCreateInfo.attachmentCount = attachments.size();
-			frameBufferCreateInfo.pAttachments = attachments.data();
-			frameBufferCreateInfo.width = m_width;
-			frameBufferCreateInfo.height = m_height;
-			frameBufferCreateInfo.layers = 1;
-			CALL_VK_FUNCTION_BREAK(vkCreateFramebuffer(m_pDevice->GetDevice(), &frameBufferCreateInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkFrameBuffer));
-
-			return true;
-		} while (false);
+	if (ptrRenderPass == nullptr) {
+		return false;
 	}
-	Destroy(true);
-	return false;
+
+	eastl::vector<VkImageView> attachments;
+	{
+		for (int indexAttachment = 0; indexAttachment < m_ptrAttachmentTextures.size(); indexAttachment++) {
+			attachments.emplace_back(((CVKRenderTexture*)m_ptrAttachmentTextures[indexAttachment].GetPointer())->GetImageView());
+		}
+
+		if (attachments.empty()) {
+			return false;
+		}
+	}
+
+	VkFramebufferCreateInfo frameBufferCreateInfo = {};
+	frameBufferCreateInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+	frameBufferCreateInfo.pNext = nullptr;
+	frameBufferCreateInfo.flags = 0;
+	frameBufferCreateInfo.renderPass = ((CVKRenderPass*)ptrRenderPass.GetPointer())->GetRenderPass();
+	frameBufferCreateInfo.attachmentCount = attachments.size();
+	frameBufferCreateInfo.pAttachments = attachments.data();
+	frameBufferCreateInfo.width = m_width;
+	frameBufferCreateInfo.height = m_height;
+	frameBufferCreateInfo.layers = 1;
+	CALL_VK_FUNCTION_RETURN_BOOL(vkCreateFramebuffer(m_pDevice->GetDevice(), &frameBufferCreateInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkFrameBuffer));
+
+	return true;
 }
 
 void CVKFrameBuffer::Destroy(bool bClear /*= true*/)
@@ -122,118 +117,3 @@ const CGfxRenderTexturePtr CVKFrameBuffer::GetAttachmentTexture(int indexAttachm
 		return nullptr;
 	}
 }
-
-/*
-CVKFrameBuffer::CVKFrameBuffer(CVKDevice* pDevice, CVKFrameBufferManager* pManager, int width, int height, int numAttachments)
-	: CGfxFrameBuffer(width, height, numAttachments)
-	, m_pDevice(pDevice)
-	, m_pManager(pManager)
-
-	, m_vkFrameBuffer(VK_NULL_HANDLE)
-
-	, m_width(width)
-	, m_height(height)
-	, m_ptrAttachmentTextures(numAttachments)
-{
-
-}
-
-CVKFrameBuffer::~CVKFrameBuffer(void)
-{
-	Destroy(true);
-}
-
-void CVKFrameBuffer::Release(void)
-{
-	m_pManager->Destroy(this);
-}
-
-VkFramebuffer CVKFrameBuffer::GetFrameBuffer(void)
-{
-	ASSERT(m_vkFrameBuffer);
-	return m_vkFrameBuffer;
-}
-
-int CVKFrameBuffer::GetWidth(void) const
-{
-	return m_width;
-}
-
-int CVKFrameBuffer::GetHeight(void) const
-{
-	return m_height;
-}
-
-bool CVKFrameBuffer::Create(const CGfxRenderPassPtr ptrRenderPass)
-{
-	Destroy(false);
-	{
-		do {
-			ASSERT(ptrRenderPass);
-
-			eastl::vector<VkImageView> attachments;
-			{
-				for (int indexAttachment = 0; indexAttachment < m_ptrAttachmentTextures.size(); indexAttachment++) {
-					attachments.emplace_back(((CVKRenderTexture*)m_ptrAttachmentTextures[indexAttachment].GetPointer())->GetImageView());
-				}
-
-				if (attachments.empty()) {
-					break;
-				}
-			}
-
-			VkFramebufferCreateInfo frameBufferCreateInfo = {};
-			frameBufferCreateInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
-			frameBufferCreateInfo.pNext = nullptr;
-			frameBufferCreateInfo.flags = 0;
-			frameBufferCreateInfo.renderPass = ((CVKRenderPass*)ptrRenderPass.GetPointer())->GetRenderPass();
-			frameBufferCreateInfo.attachmentCount = attachments.size();
-			frameBufferCreateInfo.pAttachments = attachments.data();
-			frameBufferCreateInfo.width = m_width;
-			frameBufferCreateInfo.height = m_height;
-			frameBufferCreateInfo.layers = 1;
-			CALL_VK_FUNCTION_BREAK(vkCreateFramebuffer(m_pDevice->GetDevice(), &frameBufferCreateInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkFrameBuffer));
-
-			return true;
-		} while (false);
-	}
-	Destroy(true);
-	return false;
-}
-
-void CVKFrameBuffer::Destroy(bool bClear)
-{
-	if (bClear) {
-		m_ptrAttachmentTextures.clear();
-	}
-
-	if (m_vkFrameBuffer) {
-		vkDestroyFramebuffer(m_pDevice->GetDevice(), m_vkFrameBuffer, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks());
-	}
-
-	m_vkFrameBuffer = VK_NULL_HANDLE;
-}
-
-bool CVKFrameBuffer::SetAttachmentTexture(int indexAttachment, const CGfxRenderTexturePtr ptrAttachmentTexture)
-{
-	ASSERT(ptrAttachmentTexture);
-
-	if (indexAttachment >= 0 && indexAttachment < m_ptrAttachmentTextures.size() && ptrAttachmentTexture->GetWidth() == m_width && ptrAttachmentTexture->GetHeight() == m_height) {
-		m_ptrAttachmentTextures[indexAttachment] = ptrAttachmentTexture;
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-const CGfxRenderTexturePtr CVKFrameBuffer::GetAttachmentTexture(int indexAttachment) const
-{
-	if (indexAttachment >= 0 && indexAttachment < m_ptrAttachmentTextures.size()) {
-		return m_ptrAttachmentTextures[indexAttachment];
-	}
-	else {
-		return nullptr;
-	}
-}
-*/
--- Engine/Renderer/Vulkan/src/VKRenderPass.cpp
+++ Engine/Renderer/Vulkan/src/VKRenderPass.cpp
@@ -36,142 +36,142 @@ VkRenderPass CVKRenderPass::GetRenderPass(void) const
 
 bool CVKRenderPass::Create(void)
 {
-	Destroy(false);
+	struct SubpassAttachment {
+		eastl::vector<VkAttachmentReference> inputAttachments;
+		eastl::vector<VkAttachmentReference> outputAttachments;
+		eastl::vector<VkAttachmentReference> resolveAttachments;
+		eastl::vector<uint32_t> preserveAttachments;
+		VkAttachmentReference depthStencilAttachment;
+	};
+
+	eastl::unordered_map<int, SubpassAttachment> subpassAttachments;
 	{
-		do {
-			struct SubpassAttachment {
-				eastl::vector<VkAttachmentReference> inputAttachments;
-				eastl::vector<VkAttachmentReference> outputAttachments;
-				eastl::vector<VkAttachmentReference> resolveAttachments;
-				eastl::vector<uint32_t> preserveAttachments;
-				VkAttachmentReference depthStencilAttachment;
-			};
+		for (int indexSubpass = 0; indexSubpass < m_subpasses.size(); indexSubpass++) {
+			for (const auto& itInputAttachment : m_subpasses[indexSubpass].inputAttachments) {
+				if (CGfxHelper::IsFormatColor(m_attachments[itInputAttachment.first].format)) {
+					VkAttachmentReference attachment = {};
+					attachment.attachment = itInputAttachment.first;
+					attachment.layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+					subpassAttachments[indexSubpass].inputAttachments.emplace_back(attachment);
+				}
+				else {
+					VkAttachmentReference attachment = {};
+					attachment.attachment = itInputAttachment.first;
+					attachment.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
+					subpassAttachments[indexSubpass].inputAttachments.emplace_back(attachment);
+				}
+			}
 
-			eastl::unordered_map<int, SubpassAttachment> subpassAttachments;
-			{
-				for (int indexSubpass = 0; indexSubpass < m_subpasses.size(); indexSubpass++) {
-					for (const auto& itInputAttachment : m_subpasses[indexSubpass].inputAttachments) {
-						if (CGfxHelper::IsFormatColor(m_attachments[itInputAttachment.first].format)) {
-							VkAttachmentReference attachment = {};
-							attachment.attachment = itInputAttachment.first;
-							attachment.layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-							subpassAttachments[indexSubpass].inputAttachments.emplace_back(attachment);
-						}
-						else {
-							VkAttachmentReference attachment = {};
-							attachment.attachment = itInputAttachment.first;
-							attachment.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
-							subpassAttachments[indexSubpass].inputAttachments.emplace_back(attachment);
-						}
-					}
+			for (const auto& itOutputAttachment : m_subpasses[indexSubpass].outputAttachments) {
+				// Note: Layout for color attachment can only be VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL or VK_IMAGE_LAYOUT_GENERAL.
+				VkAttachmentReference attachment = {};
+				attachment.attachment = itOutputAttachment.first;
+				attachment.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL; // VK_IMAGE_LAYOUT_GENERAL
+				subpassAttachments[indexSubpass].outputAttachments.emplace_back(attachment);
+			}
 
-					for (const auto& itOutputAttachment : m_subpasses[indexSubpass].outputAttachments) {
-						// Note: Layout for color attachment can only be VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL or VK_IMAGE_LAYOUT_GENERAL.
+			if (m_subpasses[indexSubpass].resolveAttachments.empty() == false) {
+				// Note: resolveAttachments is an optional array of outputAttachments size
+				if (m_subpasses[indexSubpass].resolveAttachments.size() != m_subpasses[indexSubpass].outputAttachments.size()) {
+					return false;
+				}
+
+				for (const auto& itResolveAttachment : m_subpasses[indexSubpass].resolveAttachments) {
+					if (CGfxHelper::IsFormatColor(m_attachments[itResolveAttachment.first].format)) {
 						VkAttachmentReference attachment = {};
-						attachment.attachment = itOutputAttachment.first;
-						attachment.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL; // VK_IMAGE_LAYOUT_GENERAL
-						subpassAttachments[indexSubpass].outputAttachments.emplace_back(attachment);
+						attachment.attachment = itResolveAttachment.first;
+						attachment.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+						subpassAttachments[indexSubpass].resolveAttachments.emplace_back(attachment);
 					}
-
-					if (m_subpasses[indexSubpass].resolveAttachments.empty() == false) {
-						// Note: resolveAttachments is an optional array of outputAttachments size
-						if (m_subpasses[indexSubpass].resolveAttachments.size() != m_subpasses[indexSubpass].outputAttachments.size()) {
-							ASSERT(false);
-							break;
-						}
-
-						for (const auto& itResolveAttachment : m_subpasses[indexSubpass].resolveAttachments) {
-							if (CGfxHelper::IsFormatColor(m_attachments[itResolveAttachment.first].format)) {
-								VkAttachmentReference attachment = {};
-								attachment.attachment = itResolveAttachment.first;
-								attachment.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-								subpassAttachments[indexSubpass].resolveAttachments.emplace_back(attachment);
-							}
-							else {
-								VkAttachmentReference attachment = {};
-								attachment.attachment = itResolveAttachment.first;
-								attachment.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-								subpassAttachments[indexSubpass].resolveAttachments.emplace_back(attachment);
-							}
-						}
-					}
-
-					for (const auto& itPreserveAttachment : m_subpasses[indexSubpass].preserveAttachments) {
-						subpassAttachments[indexSubpass].preserveAttachments.emplace_back(itPreserveAttachment.first);
-					}
-
-					if (m_subpasses[indexSubpass].depthStencilAttachment >= 0) {
+					else {
 						VkAttachmentReference attachment = {};
-						attachment.attachment = m_subpasses[indexSubpass].depthStencilAttachment;
+						attachment.attachment = itResolveAttachment.first;
 						attachment.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-						subpassAttachments[indexSubpass].depthStencilAttachment = attachment;
+						subpassAttachments[indexSubpass].resolveAttachments.emplace_back(attachment);
 					}
 				}
 			}
 
-			eastl::vector<VkAttachmentDescription> attachments;
-			eastl::vector<VkSubpassDescription> subpasses;
-			eastl::vector<VkSubpassDependency> dependencies;
-			{
-				for (int indexAttachment = 0; indexAttachment < m_attachments.size(); indexAttachment++) {
-					VkAttachmentDescription attachment = {};
-					attachment.flags = 0;
-					attachment.format = (VkFormat)m_attachments[indexAttachment].format;
-					attachment.samples = CVKHelper::TranslateSampleCount(m_attachments[indexAttachment].samples);
-					attachment.loadOp = m_attachments[indexAttachment].bClear ? VK_ATTACHMENT_LOAD_OP_CLEAR : VK_ATTACHMENT_LOAD_OP_LOAD;
-					attachment.storeOp = m_attachments[indexAttachment].bInvalidation ? VK_ATTACHMENT_STORE_OP_DONT_CARE : VK_ATTACHMENT_STORE_OP_STORE;
-					attachment.stencilLoadOp = m_attachments[indexAttachment].bClear ? VK_ATTACHMENT_LOAD_OP_CLEAR : VK_ATTACHMENT_LOAD_OP_LOAD;
-					attachment.stencilStoreOp = m_attachments[indexAttachment].bInvalidation ? VK_ATTACHMENT_STORE_OP_DONT_CARE : VK_ATTACHMENT_STORE_OP_STORE;
-					attachment.initialLayout = VK_IMAGE_LAYOUT_GENERAL;
-					attachment.finalLayout = VK_IMAGE_LAYOUT_GENERAL;
-					attachments.emplace_back(attachment);
-				}
-
-				for (int indexSubpass = 0; indexSubpass < m_subpasses.size(); indexSubpass++) {
-					VkSubpassDescription subpass = {};
-					subpass.flags = 0;
-					subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-					subpass.inputAttachmentCount = subpassAttachments[indexSubpass].inputAttachments.size();
-					subpass.pInputAttachments = subpassAttachments[indexSubpass].inputAttachments.data();
-					subpass.colorAttachmentCount = subpassAttachments[indexSubpass].outputAttachments.size();
-					subpass.pColorAttachments = subpassAttachments[indexSubpass].outputAttachments.data();
-					subpass.pResolveAttachments = subpassAttachments[indexSubpass].resolveAttachments.data();
-					subpass.preserveAttachmentCount = subpassAttachments[indexSubpass].preserveAttachments.size();
-					subpass.pPreserveAttachments = subpassAttachments[indexSubpass].preserveAttachments.data();
-					subpass.pDepthStencilAttachment = m_subpasses[indexSubpass].depthStencilAttachment >= 0 ? &subpassAttachments[indexSubpass].depthStencilAttachment : nullptr;
-					subpasses.emplace_back(subpass);
-				}
-
-				for (int indexSubpass = 1; indexSubpass < m_subpasses.size(); indexSubpass++) {
-					VkSubpassDependency dependency = {};
-					dependency.srcSubpass = indexSubpass - 1;
-					dependency.dstSubpass = indexSubpass;
-					dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
-					dependency.dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
-					dependency.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-					dependency.dstAccessMask = VK_ACCESS_INPUT_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT;
-					dependency.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
-					dependencies.emplace_back(dependency);
-				}
+			for (const auto& itPreserveAttachment : m_subpasses[indexSubpass].preserveAttachments) {
+				subpassAttachments[indexSubpass].preserveAttachments.emplace_back(itPreserveAttachment.first);
 			}
 
-			VkRenderPassCreateInfo createInfo = {};
-			createInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-			createInfo.pNext = nullptr;
-			createInfo.flags = 0;
-			createInfo.attachmentCount = attachments.size();
-			createInfo.pAttachments = attachments.data();
-			createInfo.subpassCount = subpasses.size();
-			createInfo.pSubpasses = subpasses.data();
-			createInfo.dependencyCount = dependencies.size();
-			createInfo.pDependencies = dependencies.data();
-			CALL_VK_FUNCTION_BREAK(vkCreateRenderPass(m_pDevice->GetDevice(), &createInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkRenderPass));
-
-			return true;
-		} while (false);
+			if (m_subpasses[indexSubpass].depthStencilAttachment >= 0) {
+				VkAttachmentReference attachment = {};
+				attachment.attachment = m_subpasses[indexSubpass].depthStencilAttachment;
+				attachment.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+				subpassAttachments[indexSubpass].depthStencilAttachment = attachment;
+			}
+		}
 	}
-	Destroy(true);
-	return false;
+
+	eastl::vector<VkAttachmentDescription> attachments;
+	eastl::vector<VkSubpassDescription> subpasses;
+	eastl::vector<VkSubpassDependency> dependencies;
+	{
+		for (int indexAttachment = 0; indexAttachment < m_attachments.size(); indexAttachment++) {
+			VkAttachmentDescription attachment = {};
+			attachment.flags = 0;
+			attachment.format = (VkFormat)m_attachments[indexAttachment].format;
+			attachment.samples = CVKHelper::TranslateSampleCount(m_attachments[indexAttachment].samples);
+			attachment.loadOp = m_attachments[indexAttachment].bClear ? VK_ATTACHMENT_LOAD_OP_CLEAR : VK_ATTACHMENT_LOAD_OP_LOAD;
+			attachment.storeOp = m_attachments[indexAttachment].bInvalidation ? VK_ATTACHMENT_STORE_OP_DONT_CARE : VK_ATTACHMENT_STORE_OP_STORE;
+			attachment.stencilLoadOp = m_attachments[indexAttachment].bClear ? VK_ATTACHMENT_LOAD_OP_CLEAR : VK_ATTACHMENT_LOAD_OP_LOAD;
+			attachment.stencilStoreOp = m_attachments[indexAttachment].bInvalidation ? VK_ATTACHMENT_STORE_OP_DONT_CARE : VK_ATTACHMENT_STORE_OP_STORE;
+			attachment.initialLayout = VK_IMAGE_LAYOUT_GENERAL;
+			attachment.finalLayout = VK_IMAGE_LAYOUT_GENERAL;
+			attachments.emplace_back(attachment);
+		}
+
+		for (int indexSubpass = 0; indexSubpass < m_subpasses.size(); indexSubpass++) {
+			VkSubpassDescription subpass = {};
+			subpass.flags = 0;
+			subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+			subpass.inputAttachmentCount = subpassAttachments[indexSubpass].inputAttachments.size();
+			subpass.pInputAttachments = subpassAttachments[indexSubpass].inputAttachments.data();
+			subpass.colorAttachmentCount = subpassAttachments[indexSubpass].outputAttachments.size();
+			subpass.pColorAttachments = subpassAttachments[indexSubpass].outputAttachments.data();
+			subpass.pResolveAttachments = subpassAttachments[indexSubpass].resolveAttachments.data();
+			subpass.preserveAttachmentCount = subpassAttachments[indexSubpass].preserveAttachments.size();
+			subpass.pPreserveAttachments = subpassAttachments[indexSubpass].preserveAttachments.data();
+			subpass.pDepthStencilAttachment = m_subpasses[indexSubpass].depthStencilAttachment >= 0 ? &subpassAttachments[indexSubpass].depthStencilAttachment : nullptr;
+			subpasses.emplace_back(subpass);
+		}
+
+		for (int indexSubpass = 1; indexSubpass < m_subpasses.size(); indexSubpass++) {
+			VkSubpassDependency dependency = {};
+			dependency.srcSubpass = indexSubpass - 1;
+			dependency.dstSubpass = indexSubpass;
+			dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
+			dependency.dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+			dependency.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+			dependency.dstAccessMask = VK_ACCESS_INPUT_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT;
+			dependency.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+			dependencies.emplace_back(dependency);
+		}
+
+		if (attachments.empty()) {
+			return false;
+		}
+
+		if (subpasses.empty()) {
+			return false;
+		}
+	}
+
+	VkRenderPassCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	createInfo.pNext = nullptr;
+	createInfo.flags = 0;
+	createInfo.attachmentCount = attachments.size();
+	createInfo.pAttachments = attachments.data();
+	createInfo.subpassCount = subpasses.size();
+	createInfo.pSubpasses = subpasses.data();
+	createInfo.dependencyCount = dependencies.size();
+	createInfo.pDependencies = dependencies.data();
+	CALL_VK_FUNCTION_RETURN_BOOL(vkCreateRenderPass(m_pDevice->GetDevice(), &createInfo, m_pDevice->GetInstance()->GetAllocator()->GetAllocationCallbacks(), &m_vkRenderPass));
+
+	return true;
 }
 
 void CVKRenderPass::Destroy(bool bClear /*= true*/)
